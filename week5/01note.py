46f1adb8e532e631a91dad86a9580bdde93037ab
rlsoderberg <rsoderberg20@my.whitworth.edu>
2022-11-08 11:12:42 -0800

    Looked for superbasic emailstuff, still notworking

--- week5/01note.py
+++ week5/01note.py
@@ -1,2 +1,9 @@
 note = open('note.txt', 'w')
 note.write('Hey, you totally need to figure out this email thing. And what were you going to call that Fawlty Towers video game???')
+note.close
+
+import smtplib
+
+smtpObj = smtplib.SMTP(["smtp-mail.outlook.com", 587])
+
+smtpObj.sendmail("rlsoderberg@outlook.com", "rlsoderberg@outlook.com", "note")
