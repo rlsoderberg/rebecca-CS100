5418b04b62516d26fc93baa595cde81d91f96ca9
rlsoderberg <rsoderberg20@my.whitworth.edu>
2022-12-02 14:26:33 -0800

    wait a sec, this whole exam is somethin else, redo

---
+++ cs100b/module2/school.py
@@ -0,0 +1,11 @@
+class School:
+    def __init__(self, name, location):
+        self._name = name
+        self._location = location
+        
+    def showname(self):
+        print(self.name)
+        
+    def showlocation(self):
+        print(self.location)
+        
