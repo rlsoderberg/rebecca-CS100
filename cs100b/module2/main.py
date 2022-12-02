a5e627cc52fa74ca4c569eb63b0a1c39fc7b5556
rlsoderberg <rsoderberg20@my.whitworth.edu>
2022-12-02 12:36:26 -0800

    stoked for examples. will take focus, lunch first

--- cs100b/module2/main.py
+++ cs100b/module2/main.py
@@ -7,15 +7,9 @@ def main():
     s = Student("Webfin")
     d = Driver("Pungap")
 
-    t.school.name = "Lakewood"
-    s.school = "Sunnydale"
-    s.writing_implement = "pen"
-    s.favorite_animal = "capybaras"
-    d.favorite_food = "chimichangas"
-
-    t.write_syllabus("chemistry", t.school)
-    s.coloring_book(s.writing_implement,s.favorite_animal,s.school)
-    d.foodmap(d.favorite_food)
+    t.write_syllabus("chemistry", "Lakewood")
+    s.coloring_book("pen","capybaras","Sunnydale")
+    d.foodmap("chimichangas")
 
 if __name__ == "main":
     main()
