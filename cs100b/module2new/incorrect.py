0f9d425d3a172f1ed067e7a8a81555f13e2755a7
rlsoderberg <rsoderberg20@my.whitworth.edu>
2022-12-05 10:30:10 -0800

    turtle opens minimized, plus bunch of other errors

--- cs100b/module2new/incorrect.py
+++ cs100b/module2new/incorrect.py
@@ -2,23 +2,19 @@ import threading
 import turtle
 import time
 from functools import partial
-import winsound
 screen_width = 600
 screen_height = 500
- 
- 
-def sound(num):
+
+def sound():
 	from playsound import playsound
 	playsound('klaxon.wav')
- 
- 
-def turtley(num):
+
+def test2(name):
     screen = turtle.Screen()
     screen.setup(screen_width, screen_height)
     screen.tracer(0)
     screen.title('Incorrect')
     screen.bgcolor('#111111')
-
 
     class TextBox:
         def __init__(self, x=-150, y=75, w=300, h=50, drawing_pen: turtle.Turtle = None):
@@ -33,29 +29,12 @@ def turtley(num):
                 self.pen.hideturtle()
                 self.pen.penup()
                 self.pen.color('white')
+
             self.text = ''
             self.text_size = int(self.h * 0.4)
             self.blink_timer = time.time()
             self.is_cursor_visible = True
             self.active = False
-
-        def add_letter(self, letter: str):
-            if not self.active:
-                return
-            if len(self.text) <= self.w // self.text_size:
-                self.text += letter
-
-        def remove_letter(self):
-            if not self.active:
-                return
-            self.text = self.text[0: -1]
-
-        def change_active_state(self, x, y):
-            self.active = False
-            if self.x <= x <= self.x + self.w:
-                if self.y - self.h <= y <= self.y:
-                    self.active = True
-                    return
 
         def update(self):
             if time.time() - self.blink_timer > 0.5:
@@ -66,7 +45,7 @@ def turtley(num):
             self.pen.clear()
             self.active
             if self.is_cursor_visible:
-                text = "apple"
+                text = name
             else:
                 text = self.text + ' '
             self.pen.penup()
@@ -82,15 +61,19 @@ def turtley(num):
         screen.update()
         time.sleep(0.01)
 
-
- 
- 
 if __name__ =="__main__":
-    t1 = threading.Thread(target=sound, args=(10,))
-    t2 = threading.Thread(target=turtley, args=(10,))
- 
-    t1.start()
-    t2.start()
- 
-    t1.join()
-    t2.join()
+    answer = input("what is your favorite animal?")
+    list = ["cat", "dog", "bird", "fish"]
+    if answer in list:         
+        t1 = threading.Thread(target=sound)
+        t2 = threading.Thread(target=test2, args=(answer,))
+    
+        t1.start()
+        t2.start()
+    
+        t1.join()
+        t2.join()
+    elif (answer not in list) and (answer != "capybara"):
+        print("incorrect, try again")
+    elif answer == "capybara":
+        print("that is correct")
