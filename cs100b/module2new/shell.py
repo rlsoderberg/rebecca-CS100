47df2704dc386583fadec08ab75b4cd5d35f2ee4
rlsoderberg <rsoderberg20@my.whitworth.edu>
2022-12-06 13:33:44 -0800

    well, now it gets weird on second incorrect!!!

--- cs100b/module2new/shell.py
+++ cs100b/module2new/shell.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.append("C:..\..")
 from i import *
+import turtle
 
 type = "str"
 blanky = 1
@@ -10,14 +11,22 @@ answer = main(type, blanky)
 
 list = ["cat", "dog", "bird", "fish"]
 
-while (answer not in list) and (answer != "capybara"):
-    print("incorrect, try again")
-    answer = main(type, blanky)
-if answer in list:         
-    import incorrect
-    incorrect.drawstuff(answer)
-elif answer == "capybara":
-        print("that is correct")
+
+while answer != "capybara":
+    if answer in list:         
+        import incorrect
+        incorrect.drawstuff(answer)
+        turtle.bye()
+        print("Incorrect, try again.")
+        print("What is your favorite animal?")
+        answer = main(type, blanky)
+    elif answer == "capybara":
+            print("that is correct")
+    else:
+        print("Incorrect, try again.")
+        print("What is your favorite animal?")
+        answer = main(type, blanky)
+    
 
 
 
