8592ba349084a052363853e410848ed3943aa506
rlsoderberg <rsoderberg20@my.whitworth.edu>
2022-12-06 11:37:03 -0800

    need to stop vscode, what is commits. +bad quit

--- cs100b/module2new/shell.py
+++ cs100b/module2new/shell.py
@@ -1,8 +1,21 @@
-name = input("hello diversion, may i have your name: ")
-print("is your name ",name,"?")
-correct = input()
-if correct == "yes":
-    print(len(name))
-    answer = input("what is the answer?")
-import incorrect
-incorrect.drawstuff(answer)
+#input functions
+import sys
+sys.path.append("C:..\..")
+from i import *
+
+type = "str"
+blanky = 1
+print("what is your favorite animal?")
+answer = main(type, blanky)
+
+list = ["cat", "dog", "bird", "fish"]
+
+while (answer not in list) and (answer != "capybara"):
+    print("incorrect, try again")
+    answer = main(type, blanky)
+if answer in list:         
+    import incorrect
+    incorrect.drawstuff(answer)
+elif answer == "capybara":
+        print("that is correct")
+
