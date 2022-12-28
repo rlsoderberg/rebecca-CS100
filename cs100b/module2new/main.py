b8b8a39f857ebda92517199d5e163db13e3195ca
rlsoderberg <rsoderberg20@my.whitworth.edu>
2022-12-28 15:28:46 -0800

    what even am i doing

--- cs100b/module2new/main.py
+++ cs100b/module2new/main.py
@@ -7,8 +7,7 @@ from multichoice import Multichoice
 from fillin import FillInTheBlank
 from truefalse import TrueFalse
 
-from quizquestion import checkAnswer
-
+from quizquestion import QuizQuestion
 
 #input functions
 import sys
@@ -35,76 +34,41 @@ def selecto():
     type = maine(type, blanky)
     while type != 'student' and type != 'teacher' and type!= 'driver':
         print("invalid input.")
-        print("are you a student, a teacher, or a driver?")
-        type = maine(type, blanky)
-        if type == 'student':
-            b = 0
-        elif type == 'teacher':
-            b = 1
-        elif type == 'driver':
-            b = 2
+        print("are you 1. a student, 2. a teacher, or 3. a driver?")
+        typenum = maine(type, blanky)
+        while typenum != 0:
+            print("that is wrong")
+            type = maine(type, blanky)
 
-    return b
+    return typenum
         
 
 def checkio(answ):
-    if checkAnswer(answ) == True:
+    if QuizQuestion.checkAnswer(answ) == True:
         print('well done')
     else:
         print('incorrect, sorry')
 
 def student(num):
     if num == 0:
-        showQuestion(num + 1) #??????????????
+        QuizQuestion.showQuestion(num + 1)
         mc = Multichoice("what is your favorite animal?", '3', ['beetle', 'octopus', 'capybara', 'barracuda'])
         checkio(mc)
     elif num == 1:
+        QuizQuestion.showQuestion(num + 1)
         tf = TrueFalse('you will color a picture of '+mc, '1')
         checkio(tf)
     elif num == 2:
+        QuizQuestion.showQuestion(num + 1)
         fib = FillInTheBlank('You color in the'+mc+' using a _______________ writing implement','sharpie',10)
         checkio(fib)
-
-def teacher(num):
-    print("teacher does not exist")
-    """
-        if num == 0:
-            mc = Multichoice("what is your favorite beverage?", '3', ['eggnog', 'whiskey', 'coca cola', 'ginger ale'])
-            checkio(mc)
-        elif num == 1:
-            tf = TrueFalse('you will water your plants with '+mc, '2')
-            checkio(tf)
-        elif num == 2:
-            fib = FillInTheBlank('you decide to water your _______________ with the'+mc+'. ','desk',10)
-            checkio(fib)
-        """
-
-def driver(num):
-    print("driver does not exist")
-    """
-    if num == 0:
-        mc = Multichoice('what is your favorite food', '4', ['banana', 'ravioli','broccoli','enchilada'])
-        checkio(mc)
-    elif num == 1:
-        tf = TrueFalse('you want a map to find '+mc, '1')
-        checkio(tf)
-    elif num == 2:
-        fib = FillInTheBlank('There is _______________ roadwork between you and the nearest'+mc+'restaurant.','asphalt melting',10)
-        checkio(fib)
-    """
 
 def main():
     nums = [0,1,2]
     type = selecto()
     if type == 0:
         for b in nums:
-            student()
-    elif type == 1:
-        for b in nums:
-            teacher()
-    elif type == 2:
-        for b in nums:
-            driver()
+            student(b)
 
     
 
