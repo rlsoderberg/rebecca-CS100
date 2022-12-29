c6ae7d9b71930fec0578983ef7fb5c99863cbc53
rlsoderberg <rsoderberg20@my.whitworth.edu>
2022-12-28 22:32:09 -0800

    haha wat was i doing. still need Multichoice = str

--- cs100b/module2new/main.py
+++ cs100b/module2new/main.py
@@ -28,8 +28,7 @@ def persona():
     s.sleep()
     t.eat()
 
-
-def selecto():
+def examtype():
     print("are you 1. a student, 2. a teacher, or 3. a driver?")
     typo = maine(type, blanky)
     if typo == '1':
@@ -44,39 +43,36 @@ def selecto():
             print("are you 1. a student, 2. a teacher, or 3. a driver?")
             typo = maine(type, blanky)
 
-    return typo
-        
-
-def checkio(answ):
-    if QuizQuestion.checkAnswer(answ) == True:
-        print('well done')
-    else:
-        print('incorrect, sorry')
-
-def student(num):
-    propnum = num + 1
-    if num == 0:
-        QuizQuestion.showQuestion(propnum)
-        mc = Multichoice("what is your favorite animal?", '3', ['beetle', 'octopus', 'capybara', 'barracuda'])
-        checkio(mc)
-    elif num == 1:
-        QuizQuestion.showQuestion(propnum)
-        tf = TrueFalse('you will color a picture of '+mc, '1')
-        checkio(tf)
-    elif num == 2:
-        QuizQuestion.showQuestion(propnum)
-        fib = FillInTheBlank('You color in the'+mc+' using a _______________ writing implement','sharpie',10)
-        checkio(fib)
 
 def main():
-    nums = [0,1,2]
-    selectoni = selecto()
-    for b in nums:
-        student(b)
+    t = Teacher('smith')
+    s = Student('jones')
 
+    s.sleep()
+    t.eat()
     
+    smc = Multichoice("what is your favorite animal?", '3', ['beetle', 'octopus', 'capybara', 'barracuda'])
+    stf = TrueFalse('you will color a picture of '+smc, '1')
+    sfib = FillInTheBlank('You color in the'+smc+' using a _______________ writing implement','sharpie',10)
 
+    tmc = Multichoice("what is your favorite beverage?", '3', ['eggnog', 'whiskey', 'coca cola', 'ginger ale']) 
+    ttf = TrueFalse('you will water your plants with '+tmc, '2')
+    tfib = FillInTheBlank('you decide to water your _______________ with the'+tmc+'. ','desk',10)
 
+    dmc = Multichoice('what is your favorite food', '4', ['banana', 'ravioli','broccoli','enchilada'])
+    dtf = TrueFalse('you want a map to find '+dmc, '1')
+    dfib = fib = FillInTheBlank('There is _______________ roadwork between you and the nearest'+dmc+'restaurant.','asphalt melting',10)
+
+    exam = [(smc, stf, sfib),(tmc, ttf, tfib),(dmc, dtf, dfib)]
+    
+    for i in range(0, len(exam)):
+        t = examtype()
+        exam[i].showQuestion(i+1)
+        resp = input('> ')
+        if exam[i].checkAnswer(resp) == True:
+            print('well done')
+        else:
+            print('incorrect, sorry')
 
 if __name__ == '__main__':
-   main()
+    main()
