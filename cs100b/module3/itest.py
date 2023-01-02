6342dad2b5151a81ab814e80a3f5d60e1411cc99
rlsoderberg <rsoderberg20@my.whitworth.edu>
2023-01-02 13:18:57 -0800

    started, hungup on input

---
+++ cs100b/module3/itest.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('../../')
+
+from i import *
+
+type = "str"
+blanky = 1
+x = maine(type, blanky)
+print(x)
+
---
+++ cs100b/module3/lifesaver.py
@@ -0,0 +1,33 @@
+import collections
+import random
+
+class LifeSaver:
+    def __init__(self, flavor):
+        self.flavor=flavor
+
+tube = collections.deque()
+
+import collections
+import random
+
+class LifeSaver:
+    def __init__(self, flavor):
+        self.flavor = flavor
+
+tube = collections.deque()
+flavors = ["orange","pineapple","cherry","raspberry","watermelon"]
+for l in range (0,10):
+    flavor = flavors[random.randrange(0,len(flavors))]
+    lifesaver = LifeSaver(flavor)
+    tube.append(lifesaver)
+
+while len(tube)>0:
+    side = random.randrange(0,2)
+    if side ==0:
+        l = tube.pop()
+        print("eating " + l.flavor + " from the right side")
+    else:
+        l = tube.popleft()
+        print("eating "+l.flavor+" from the left side")
+
+print("YUM!")
