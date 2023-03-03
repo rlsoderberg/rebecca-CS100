d674250e6845c96520c5fcef61b66cdc6bfddb05
rlsoderberg <rsoderberg20@my.whitworth.edu>
2023-03-02 18:51:52 -0800

    made comments starting at bottom.still need to fix

--- cs100a/module1/01math.py
+++ cs100a/module1/01math.py
@@ -11,180 +11,85 @@ def int_gen():
 def float_gen():
     return random.uniform(0, 9.9)
 
-def correctness(guess):
-    if int_vars[<secret number>] == guess:
-        correctness = 1
-    elif int_vars[<secret number>] != guess:
-        correctness = 2
+def correctness(guess, prob_num):
+    if int_vars[prob_num] == guess:
+        return 1
+    elif int_vars[prob_num] != guess:
+        return 2
     
+#very much correctness
+def get_answer(x, prob_num):
+    print(x + " = ")
+    type = "string"
+    antiblanky = 1
+    guess = maine(type, antiblanky)
+    correct = correctness(guess, prob_num)
+    return correct
 
-
-def guess_variable():
+#what is going on right here with these variables?????
+def guess_variable(prob_num):
     print("which variable do you wish to guess? x, y, or z?")
     #make a filter for letters other than x, y, or z
     type = "string"
     antiblanky = 1
     var_letter = maine(type, antiblanky)
-    #see, i should totally be able to skip this part where i'm checking these variables separately, but i don't know how!!!!
     if var_letter == "x":
-    #there was totally a point in writing this program where i started referring to <secret numbers>
-    #but now i can't find it!!!
-    #<secret number> is what i want
-    #the value in int_vars that is connected with a certain var_letter
-        print("x = ")
-        type = "string"
-        antiblanky = 1
-        guess = maine(type, antiblanky)
-        #<secret number> becomes useful down here in correctness
-        correct = correctness(guess)
+        correctness = get_answer(x, prob_num)
+        return correctness
+    elif var_letter == "y":
+        correctness = get_answer(y, prob_num)
+        return correctness
+    elif var_letter == "z":
+        correctness = get_answer(z, prob_num)
         return correctness
 
-
-
-    
-    
-
-
-def int_prob(int_vars):
+#4. ok, i passed prob_num all the way up to correctness, because it looks like that might be my <secret number>
+def int_prob(int_vars, prob_num):
     print("Problem "+prob_num+": "+op_names[prob_num])
     x = int_vars[0]
     y = int_vars[1]
     z = int_vars[2]
     solution = prob_eqs[prob_num]
     print(prob_strings[prob_num] + "= "+ solution)
-    print("press 1 to guess a variable, and press 2 to continue to the next operation")
-    type = "int"
-    antiblanky = 1
-    choose_path = maine(type, antiblanky)
-    #while loop isn't what i want!!! i want to allow for incorrect guesses!!!
+    choose_path = 1
+    correct_count = 0
     while choose_path == 1:
-        correct = guess_variable()
-        if correct == 1:
-            choose_path = 2
+        print("press 1 to guess a variable, and press 2 to continue to the next operation")
+        type = "int"
+        antiblanky = 1
+        choose_path = maine(type, antiblanky)
+        if choose_path == 1:
+            correctness = guess_variable(prob_num)
+            if (correctness == 1):
+                correct_count += 1
+        elif choose_path == 2:
+            #exit
+    #why doesn't it like my prob_num?
+    prob_num += 1
         
-
-def int_array():
+#3. i have these, like, peripheral functions generating random ints. as well as the int_gens up there, which are feeding them.
+def int_array(int_vars):
     for n in range(0, 3):
         rand_int = int_gen()
         int_vars.append(rand_int)
         n+=1
+    return int_vars
 
-def float_array():
-    for n in range(0, 3):
-        longfloat = float_gen()
-        rand_float = round(longfloat, 1)
-        float_vars.append(rand_float)
-        n+=1
-
+#2. here i am, picking a type, and sending to int_prob. i am sending up my empty int array, int_vars
 def type_select(var_type, int_vars, float_vars):
-    if var_type == "int"
+    int_vars = int_array()
+    if var_type == "int":
         int_prob(int_vars)
-    elif var_type == "float"
-        float_prob(float_vars)
+
 
 var_type = "int"
 prob_num = 1
 int_vars = []
 float_vars = []
-#also why does %x always turn blue?
 prob_strings = ["x + y + x","z - y - x","x * y * z","(x+y)/z","(y-z)%x","x**z+y**z"]
 prob_eqs = [x + y + x,z - y - x,x * y * z,(x+y)/z,(y-z)%x,x**z+y**z]
 op_names = ["0", "Addition", "Subtraction","Multiplication","Division","Modulo","Exponent"]
 rand_int = 0
 rand_float = 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-def check(s):
-    #how do i pick which variable i am checking????
-    print("press 1 to guess a variable, and press 2 to continue to the next operation")
-    action = maine(type, antiblanky)
-    while action == 1: #is this even the right way to do it???
-        guess = maine(type, antiblanky)
-        if guess == #the right answer?????:
-            print("Correct!")
-        elif guess != #the right answer?????:
-            print("Incorrect!")
-        print("press 1 to guess a variable, and press 2 to continue to the next operation")
-        action = maine(type, antiblanky)
-
-
-
-
-def label(count):
-    count += 1
-    print("Operation "+count+": ")
-
-
-
-
-#welcome users to math.py
-print("Welcome to math.py. \n Can you guess the variables?")
-
-#create int variables
-x = 7
-y = 4
-z = 26
-
-#create float variables
-a = 9.3
-b = 2.6
-c = 15.7
-
-#perform arithmetic operations on int variables
-sum = x + y + z
-difference = z - y - x
-product = x * y * z
-quotient = (x + y) / z
-mod = (y-z)%x
-power = x**z + y**z
-
-count = 0
-
-#display arithmetic operations for int variables
-print("1. Arithmetic operations performed on "+ type(x)+ "variable")
-label(count)
-print("x + y + z = ", sum)
-check(s) #see, what i need to do, is put the variables in, like, a set or something? maybe also make them so i can referece them? idk!!!
-#and also, i need to be looping through the problems!!!
-# AND i need to tidy up my input function!!!
-print("x - y - z = ", difference)
-print("x * y * z = ", product)
-print("(x + y)/z = ", quotient)
-print("(y - z) % x = ", mod)
-print("x**z + y**z = ", power)
-
-#perform arithmetic operations on float variables
-sum = x + y + z
-difference = x - y - z
-product = x * y * z
-quotient = (x + y)/z
-mod = (y - z) % x
-power = x**z + y**z
-
-count = 0
-
-
-
-#display arithmetic operations for float variables
-print("1. Arithmetic operations performed with "+ type(a))+ "variable")
-print("x - y - z = ", difference)
-print("x * y * z = ", product)
-print("(x + y)/z = ", quotient)
-print("(y - z) % x = ", mod)
-print("x**z + y**z = ", power)
-
-"""
+#1. i am starting this off by picking a type (although i only have int going right now)
+type_select(var_type, int_vars, float_vars)
