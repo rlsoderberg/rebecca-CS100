32684c307853454b02f1786deb7584d4886a68d2
rlsoderberg <rsoderberg20@my.whitworth.edu>
2023-03-21 10:27:14 -0700

    still things to fix!

--- cs100a/module1/01math.py
+++ cs100a/module1/01math.py
@@ -23,49 +23,65 @@ def check_answer(abc, letter_pair, solution):
     return correctness
 
 
-def letter_guess():
+def letter_guess(intfloat):
     print("which variable do you wish to guess? x, y, or z?")
     type = "string"
     antiblanky = 1
-    var_to_guess = maine(type, antiblanky)
-    while var_to_guess != 'x' and var_to_guess != 'y' and var_to_guess != 'z':
-        var_to_guess = maine(type, antiblanky)
-    letter_pair = (0,0)
+    var_select = maine(type, antiblanky)
+    while var_select != 'x' and var_select != 'y' and var_select != 'z':
+        var_select = maine(type, antiblanky)
+    letter_pair = (0,'f')
     var_letter = maine(type, antiblanky)
     if var_letter == "x":
-        print("x = ")
+        if intfloat == 0:
+            print("INT x = ")     
+        if intfloat == 1:
+            print("FLOAT x = ") 
         letter_pair[0] = 0
         letter_pair[1] = maine(type, antiblanky)
     elif var_letter == "y":
-        print("y = ")
+        if intfloat == 0:
+            print("INT y = ")     
+        if intfloat == 1:
+            print("FLOAT y = ") 
         letter_pair[0] = 1
         letter_pair[1] = maine(type, antiblanky)
     elif var_letter == "z":
+        if intfloat == 0:
+            print("INT z = ")     
+        if intfloat == 1:
+            print("FLOAT z = ") 
         letter_pair[0] = 2
         letter_pair[1] = maine(type, antiblanky)
     return letter_pair
 
 
-def prob_display(abc, prob_strings, solution, op_names):
+def prob_display(prob_strings, solution, op_names, prob_num, intfloat):
     print("Problem "+prob_strings+": "+op_names[prob_num])
-    a = abc[0]
-    b = abc[1]
-    c = abc[2]
     print(prob_strings[prob_num] + "= "+ solution)
     choose_path = 1
     correct_count = 0
-    print("press 1 to guess a variable, and press 2 to continue to the next operation")
-    type = "int"
+
+    print("press 1 to guess:")
+    if intfloat == 0:
+        print("an INT variable")     
+    if intfloat == 1:
+        print("a FLOAT variable") 
+    print("press 2 to continue to the next equation")
     antiblanky = 1
     path_var = maine(type, antiblanky)
+    while path_var < 1 or path_var > 2:
+        path_var = maine(type, antiblanky)
     return path_var
 
-def correct_answers(prob_eqs, prob_num, abc):
+def correct_answers(prob_eqs, abc, solution):
+    x = abc[0]
+    y = abc[1]
+    z = abc[2]
     for b in abc:
-        #i don't think i'm transferring these symbols right, and i don't know if sympy is the best way to do this
+        #list index out of range . is sympy the right way to do this?????
         x, y, z = symbols('x y z')
-        d = abc[b]
-        solution = solve(d)
+        solution.append(solve(prob_eqs))
     return solution
 
 def floatgen_abc(abc):
@@ -84,27 +100,30 @@ def intgen_abc(abc):
 
 
 prob_num = 1
-prob_count = 1
 abc = []
 prob_strings = ["x + y + x","z - y - x","x * y * z","(x+y)/z","(y-z)%x","x**z+y**z"]
 x, y, z = symbols('x y z')
 prob_eqs = [(x + y + x),(z - y - x),(x * y * z),((x+y)/z),((y-z)%x),(x**z+y**z)]
 op_names = ["0", "Addition", "Subtraction","Multiplication","Division","Modulo","Exponent"]
 total_questions = 6
+#this seems somewhat inefficient!!!
+intfloat = 0
 
 while total_questions > 0:
     correct_count = 0
     abc.clear()
     if total_questions >= 3:
+        intfloat = 0
         abc = intgen_abc(abc)
     elif total_questions < 3:
+        intfloat = 1
         abc = floatgen_abc(abc)
-    solution = correct_answers(prob_eqs, prob_num, abc)
-    path_var = prob_display(abc, prob_strings, solution, op_names)
+    solution = []
+    solution = correct_answers(prob_eqs, abc, solution)
+    path_var = prob_display(prob_strings, solution, op_names, prob_num)
     while path_var == 1:
-        letter_pair = letter_guess()
+        letter_pair = letter_guess(intfloat)
         correctness = check_answer(abc, letter_pair, solution)
         correctness_display(correct_count, correctness)
-    prob_num += 1
-    prob_count += 1
     total_questions -= 1
+    prob_num += 1
