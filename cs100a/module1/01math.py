be4761eb9b861d27c34bcaec77c3d847235c4ec3
rlsoderberg <rsoderberg20@my.whitworth.edu>
2023-03-27 08:09:07 -0700

    replaced antiblanky w/ more classy prev_empty ;)

--- cs100a/module1/01math.py
+++ cs100a/module1/01math.py
@@ -6,56 +6,51 @@ from i import input_function
 import random
 from sympy import symbols, Eq, solve
 
-def correctness_display(abc, letter_pair, solutions, correct_count, op_num):
-    a = abc[0]
-    b = abc[1]
-    c = abc[2]
+def correctness_display(abc, letter_pair, correct_count, op_num):
     var_select = letter_pair[0]
     if (abc[var_select] == letter_pair[1]):
         correct_count += 1
         print("correct!")
-        print("correct answers: "+str(correct_count)+" out of "+str(op_num+ 1))
+        print("correct answers: "+str(correct_count)+" out of 3")
     elif (abc[var_select] != letter_pair[1]):
         print("incorrect!")
-        print("correct answers: "+str(correct_count)+" out of "+str(op_num + 1))
+        print("correct answers: "+str(correct_count)+" out of 3")
 
 
 def letter_guess():
     print("which variable do you wish to guess? x, y, or z?")
-    type = "string"
-    antiblanky = 1
+    type_name = "string"
+    prev_empty = 1
     var_select = "a"
     while var_select != 'x' and var_select != 'y' and var_select != 'z':
-        var_select = input_function(type, antiblanky)
+        var_select = input_function(prev_empty, type_name)
     letter_pair = (0,'f')
     if var_select == "x":
         type = "int"
         print("int x =")
-        letter_pair = (0, input_function(type, antiblanky))
+        letter_pair = (0, input_function(prev_empty, type_name))
     elif var_select == "y":
         type = "int"
         print("int y =")
-        letter_pair = (1, input_function(type, antiblanky))
+        letter_pair = (1, input_function(prev_empty, type_name))
     elif var_select == "z":
         type = "int"
         print("int z =") 
-        letter_pair = (2, input_function(type, antiblanky))
+        letter_pair = (2, input_function(prev_empty, type_name))
     return letter_pair
 
 
 def prob_display(prob_strings, solution, op_names, op_num):
     print("Operation "+str(op_num + 1)+": "+op_names[op_num])
     print(str(prob_strings[op_num]) + " = "+ str(solution))    
-    choose_path = 1
-    correct_count = 0
 
     print("press 1 to guess a variable")
     print("press 2 to return to the main menu")
-    antiblanky = 1
-    type = "int"
+    prev_empty = 1
+    type_name = "int"
     path_var = 0
     while path_var < 1 or path_var > 2:
-        path_var = input_function(type, antiblanky)
+        path_var = int(input_function(prev_empty, type_name))
     return path_var
 
 def correct_answers(prob_eqs, abc, solutions, op_num):
@@ -94,13 +89,13 @@ def problem(op_num, abc, prob_strings, prob_eqs, op_names):
     path_var = prob_display(prob_strings, solutions, op_names, op_num) 
     while path_var == 1:
         letter_pair = letter_guess()
-        correctness_display(abc, letter_pair, solutions, correct_count, op_num)
+        correctness_display(abc, letter_pair, correct_count, op_num)
         print("press 1 to guess another variable for this operation, or 2 to return to main menu.")
-        antiblanky = 1
-        type = "int"
+        prev_empty = 1
+        type_name = "int"
         path_var = 0
         while path_var < 1 or path_var > 2:
-            path_var = input_function(type, antiblanky)
+            path_var = input_function(prev_empty, type_name)
     op_num += 1
     return correct_count
 
@@ -108,11 +103,11 @@ def operation_select(correct_count):
     print("welcome to GUESS THE VARIABLE")
     print("type a symbol to pick an operation:")
     print("1. + addition   2. - subtraction   3. * multiplication   4. / division   5. % modulo   6. ^ exponent")
-    type = "string"
-    antiblanky = "1"
+    type_name = "string"
+    prev_empty = "1"
     op_input = "0"
     while op_input != "+" and op_input != "-" and op_input != "*" and op_input != "/" and op_input != "%" and op_input != "^":
-        op_input = input_function(type, antiblanky)
+        op_input = input_function(prev_empty, type_name)
     #look at all this copy and pasting!!!!!! aaaaaaaaaaa
     #also what even are integers and floats?
     if op_input == "+":
--- i.py
+++ i.py
@@ -1,103 +1,12 @@
-#look at all this copy pasting!!! can i do, like, varying criteria or something somehow???
-def blankfriendlyint():
+def input_function(prev_empty, type_name):
     valid = False
     while valid == False:
         try:
-            x = int(input('give me an int: '))
+            x = input("give me a "+str(type_name)+": ")
+            if prev_empty == 1:
+                if not x:
+                    raise ValueError('empty string')
             valid = True
         except ValueError:
             print("Invalid Input!")
-    return x
-
-def antiblankyint():
-    valid = False
-    while valid == False:
-        try:
-            x = int(input('give me an int: '))
-            valid = True
-        except ValueError:
-            print("Invalid Input!")
-        except x == "":
-            print("Invalid Input!")
-    return x
-    
-def blankfriendlyfloat():
-    valid = False
-    while valid == False:
-        try:
-            x = float(input('give me a float: '))
-            valid = True
-        except ValueError:
-            print("Invalid Input!")
-    return x
-
-def antiblankyfloat():
-    valid = False
-    while valid == False:
-        try:
-            x = float(input('give me a float: '))
-            valid = True
-        except ValueError:
-            print("Invalid Input!")
-        except x == "":
-            print("Invalid Input!")
-    return x
-
-def blankfriendlystr():
-    valid = False
-    while valid == False:
-        try:
-            x = input('give me a string: ')
-            valid = True
-        except ValueError:
-            print("Invalid Input!")
-    return x
-
-def antiblankystr():
-    valid = False
-    while valid == False:
-        try:
-            x = input('give me a string: ')
-#what??? i found this and it works, but how is 'empty string' even used???
-            if not x:
-                raise ValueError('empty string')
-            valid = True
-        except ValueError:
-            print("Invalid Input!")
-    return x
-
-def hello():
-    print("hello")
-
-def blanky_typeselect(type):
-    if type == "string":
-        x = blankfriendlystr()
-        return x
-    elif type == "float":
-        x = blankfriendlyfloat()
-        return x
-    elif type == "int":
-        x = blankfriendlyint()
-        return x
-
-def antiblanky_typeselect(type):
-    if type == "string":
-        x = antiblankystr()
-        return x
-    elif type == "float":
-        x = antiblankyfloat()
-        return x
-    elif type == "int":
-        x = antiblankyint()
-        return x
-
-def input_function(type, antiblanky):
-    ret = ""
-    if antiblanky == 1:
-        ret = antiblanky_typeselect(type)
-    else:
-        ret = blanky_typeselect(type)
-    return ret
-
-
-    
+    return x  
