be446472e92f7a1f15bd457b7b6eeb967d0aaa8f
rlsoderberg <rsoderberg20@my.whitworth.edu>
2023-03-31 16:40:02 -0700

    i think it's working?

--- i.py
+++ i.py
@@ -1,21 +1,3 @@
-#this thing isn't even working!!! aaaaaaaaa
-
-def str_input_function():
-    valid = False
-    while valid == False:
-        try:
-            x = input("please enter a nonempty string : ")
-            if not x:
-                raise ValueError('empty string')
-            valid = True
-        except ValueError:
-            print("Invalid Input!")
-    return x  
-
-def str_empty_input_function():
-    x = input("please press any key: ")
-    return x  
-
 def int_input_function():
     valid = False
     while valid == False:
@@ -38,30 +20,12 @@ def float_input_function():
             valid = True
         except ValueError:
             print("Invalid Input!")
-    return x  
-
-def float_input_function():
-    valid = False
-    while valid == False:
-        try:
-            x = input("please enter a variable: ")
-            if not x:
-                raise ValueError('empty string')
-            valid = True
-        except ValueError:
-            print("Invalid Input!")
-    return x  
+    return x   
 
 def input_function(type_name):
-    if type_name == "str":
-        input = str_input_function()
-    if type_name == "str_empty":
-        input = str_empty_input_function()
-    elif type_name == "int":
+    if type_name == "int":
         input = int_input_function()
     elif type_name == "float":
         input = float_input_function()
-    else:
-        input = str_input_function()
     return input
 
