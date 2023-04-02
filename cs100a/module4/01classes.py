15733baecbdb78214c9a3cf073a3794f19e19d18
rlsoderberg <rsoderberg20@my.whitworth.edu>
2023-04-02 16:18:32 -0700

    reset everything

--- cs100a/module4/01classes.py
+++ cs100a/module4/01classes.py
@@ -6,7 +6,7 @@ from i import input_function
 
 import datetime
 
-
+#classes
 class Student:
     name = ''
     ID = -1
@@ -24,13 +24,13 @@ class Course:
     classroom = 0
     teacher = ""
     students = []
-    
+#set class properties, and print
 stud = Student
 stud.name = "billy bob"
-stud.ID = 22342
+stud.ID = 19789291
 stud.favsport = "golf"
 stud.birthdate = '4/10/1991'
-print("we wanted to throw a  "+stud.favsport+" party for "+stud.name+" on his birthday, "+stud.birthdate+", but we got intercepted by azealia banks, who was singing her hit song "+str(stud.ID))
+print("we wanted to throw a  "+stud.favsport+" party for "+stud.name+" on his birthday, "+stud.birthdate+", but we got intercepted by birthday gnomes, who were attracted to the number"+str(stud.ID))
 teach = Teacher
 teach.name = "mr giles"
 teach.office = 202
@@ -43,6 +43,7 @@ algebra.classroom = 204
 algebra.teacher = "mr. mcneil"
 algebra.students = ["colleen fumpo", "paul leghat", "rachel stendec", "michael overgold", "ali redbuckle"]
 print(algebra.name+" Roster\nTeacher: "+algebra.teacher+"\nStudents Beginning With M:")
+#special loop of m
 for s in algebra.students:
     if s.startswith('m'):
         print(s)
