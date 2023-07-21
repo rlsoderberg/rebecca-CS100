d7b2b981f40379f4987f00ad4ba60664a9d2fe76
rlsoderberg <rsoderberg20@my.whitworth.edu>
2023-07-21 08:10:35 -0700

    there, math 01 is not breaking...

--- cs100a/module1/3math01.py
+++ cs100a/module1/3math01.py
@@ -8,7 +8,7 @@ def input_function():
     valid = False
     while valid == False:
         try:
-            x = int(input("please enter the int value of x = "))
+            x = int(input("VALUE OF X: "))
             if not x:
                 raise ValueError('empty string')
             valid = True
@@ -20,8 +20,10 @@ import random
 
 equation_strings = ["5x + 3 = 13", "14 - 3x = 2", "9 - 2x = 3", "8x + 2 = 50", "2x + 16 = 40", "12 - 2x = 2", "7 = 49 - 6x", "30 + 7x = 86", "4x + 20 = 56", "3x - 3 = 39"]
 #i should totally come up with a way to auto solve these
+#oooh, I don't know, I think I would have to use a library or something
 answers = [2, 4, 3, 6, 12, 5, 7, 8, 9, 14]
 
+#provide narration
 print("You are climbing a mountain. Suddenly, your hand slips.")
 print("There seems to be a slippery patch on the side of the mountain.")
 
@@ -55,6 +57,7 @@ key = input("Press any key to continue.")
 print("\nA drawer slides out of the mountain, beside the doorway.")
 print("TAKE 1 INFLATABLE LAVA BOAT.")
 print("An inflatable lava boat? You look quizzically at the shrivelled, oblong shape in the drawer.")
+
 valid = False
 while valid == False:
     try:
@@ -76,61 +79,61 @@ elif takeboat == "b":
 
 key = input("Press any key to continue.")
 
-print("\nYou are on a raised platform in the middle of a cavern. \nYou can see lava below you, all around the platform.")
+print("\nYou find yourself on a raised platform in the middle of a cavern. \nYou see lava below you, all around the platform.")
 print("The metallic voice reverberates within the cavern.")
 name = str(input("IDENTIFICATION PLEASE: "))
 
 rando = 0
 prob_seq = []
 
+#isn't it weird that range doesn't include the last number, but randint does? is that what is happening
 for x in range (0, 10):
     rando = random.randint(0,9)
-    #apply brute force. is this really what i should be doing??? can't i go bring back some better search function??
-    for b in range (0, 100):
-        if rando in prob_seq:
-            rando = random.randint(0,10)
+    #what was the point of that brute force exactly??? look, idk how to deal with duplicates, kind of hard to test
+    while rando in prob_seq:
+        rando = random.randint(0,9)
     prob_seq.append(rando)
 
 correct = 0
 lava_level = 0
 
-for x in range (0,9):
+#ALL CAPS EVERYTHING. is that some clipping i hear oOoOoOoH
+for x in range (0,10):
     if (x == 0):
         print("\nWELCOME, "+name.upper()+".")
         print("PLEASE INFLATE BOAT.")
         print("You see the lava beginning to rise to your level. You inflate the boat and sit down inside.")
     key = input("Press any key to continue.")
     print("\nCURRENT LAVA LEVEL: "+str(lava_level)+" FEET")
-    print("EQUATION "+str(x+1)+" OF "+name.upper()+":")
-    #wait, what the heck? i thought changing it to (0, 9) would fix it, but it's still messing up here
+    print("\nEQUATION "+str(x+1)+" OF "+name.upper()+":")
     print(str(equation_strings[prob_seq[x]]))
     user_ans = input_function()
     if user_ans == answers[prob_seq[x]]:
-        print("correct! "+name.upper()+" has found the correct value of x = "+str(answers[prob_seq[x]])+". lava level increases by "+str(100*(prob_seq[x]+10))+" feet")
+        print("CORRECT! "+name.upper()+" HAS IDENTIFIED X = "+str(answers[prob_seq[x]])+". LAVA LEVEL INCREASES BY "+str(100*(prob_seq[x]+10))+" FEET.")
         correct += 1
         lava_level += (100*(prob_seq[x]+10))
     elif user_ans != answers[prob_seq[x]]:
-        print("incorrect! "+name.upper()+" has not found the correct value of x = "+str(answers[prob_seq[x]])+". lava level decreases by "+str(50*(prob_seq[x]+10))+" feet")
+        print("INCORRECT! "+name.upper()+" HAS NOT IDENTIFIED X = "+str(answers[prob_seq[x]])+". LAVA LEVEL DECREASES BY "+str(50*(prob_seq[x]+10))+" FEET.")
         lava_level -= (50*(prob_seq[x]+10))
 
-print(name+" SOLVED "+str(correct)+" EQUATIONS CORRECTLY. \nLAVA LEVEL IS "+str(lava_level)+" FEET\n")
+print("\n"+name.upper()+" SOLVED "+str(correct)+" EQUATIONS CORRECTLY. \nLAVA LEVEL IS "+str(lava_level)+" FEET.")
+key = input("Press any key to continue.\n")
+
 if lava_level >= 10000:
     print("You are carried out the top of the mountain, and float down on an exhilarating deluge of lava.")
 elif lava_level >= 1000:
     print("You paddle through the cavern on the lava boat, and get lost.")
     if takeboat == "a":
         print("You regret wasting your hiking explosives.")
-        print("Luckily, you also have a sense for normal spelunking, and you eventually find a way out.")
+        print("You eventually find a way out, with old fasioned spelunking, no explosives.")
     else:
         print("You are very thankful that you didn't waste your explosives.")
         print("You use the explosives to defend against feral cave bats, and you eventually find a way out.")
 
 elif lava_level >= 0:
     print("You find yourself back in the vestibule of the volcano.")
-    print("With all the lava at a uniform level, you just paddle around until you get teleported out")
+    print("With all the lava at a uniform level, you just paddle around until you get teleported out.")
 else:
-    print("You are pulled down toward the center of the earth in a vortex of lava")
-
-#is there no cthulhu easter egg in here?
+    print("You are pulled down toward the center of the earth in a vortex of lava.")
 
 
