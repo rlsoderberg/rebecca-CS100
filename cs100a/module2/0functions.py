85b4b1c7385f5c2a37ec9096dc736ba11e4dca92
Rebecca Soderberg <vogindex@gmail.com>
2023-10-03 16:14:30 -0700

    now it automatically returns to menu? infuriating

--- cs100a/module2/0functions.py
+++ cs100a/module2/0functions.py
@@ -12,47 +12,53 @@ def digit_total(string):
             total += int(s)
     return total
 
-print("Would you like to:\n1) Calculate the month corresponding to an integer?\n2) Calculate the sum of the integer's digits\n3) Calculate the integer's factorial\nWhich will it be?")
 
-#well... i'm just going to make a custom input function?
-valid = False
-while valid == False:
-    try:
-        problem_choice = int(input("Please enter 1, 2, or 3: "))
-        if problem_choice not in range (0, 4):
-            raise ValueError("out of range")
-        else:
-            valid = True
-    except ValueError:
-        print("Invalid Input!")
+exit = "0"
+while exit != "x":
+    
+    print("Would you like to:\n1) Calculate the month corresponding to an integer?\n2) Calculate the sum of the integer's digits\n3) Calculate the integer's factorial\nWhich will it be?")
 
-if (problem_choice == 1):
-
-    month_number = 0
-
+    #well... i'm just going to make a custom input function?
     valid = False
     while valid == False:
         try:
-            month_number = int(input("Please enter an integer from 1 to 12: "))
-            if month_number not in range (0, 13):
+            problem_choice = int(input("Please enter 1, 2, or 3: "))
+            if problem_choice not in range (0, 4):
                 raise ValueError("out of range")
             else:
                 valid = True
         except ValueError:
             print("Invalid Input!")
-    month_name = calendar.month_name[month_number]
-    print("month "+str(month_number)+" is "+month_name)
 
-elif (problem_choice == 2):
+    if (problem_choice == 1):
 
-    string = input("please enter integer string to find its digit total: ")
-    digit_total = digit_total(string)
-    print("digit total for '"+string+"' is "+str(digit_total))
+        month_number = 0
 
-elif (problem_choice == 3):
+        valid = False
+        while valid == False:
+            try:
+                month_number = int(input("Please enter an integer from 1 to 12: "))
+                if month_number not in range (0, 13):
+                    raise ValueError("out of range")
+                else:
+                    valid = True
+            except ValueError:
+                print("Invalid Input!")
+        month_name = calendar.month_name[month_number]
+        print("month "+str(month_number)+" is "+month_name)
 
-    n = int(input("please enter integer to calculate factorial: "))
-    factorial = 1
-    for b in range (1, n+1):
-        factorial = b * factorial
-    print("factorial of "+str(n)+" is "+str(factorial))
+    elif (problem_choice == 2):
+
+        string = input("please enter integer string to find its digit total: ")
+        digit_total = digit_total(string)
+        print("digit total for '"+string+"' is "+str(digit_total))
+
+    elif (problem_choice == 3):
+
+        n = int(input("please enter integer to calculate factorial: "))
+        factorial = 1
+        for b in range (1, n+1):
+            factorial = b * factorial
+        print("factorial of "+str(n)+" is "+str(factorial))
+
+exit = input("press (almost) any key to return to main menu, or press x to exit")
