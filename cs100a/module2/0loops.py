169fc4d15b36d68847e7124882aebb77770ec5e8
Rebecca Soderberg <vogindex@gmail.com>
2023-10-03 17:06:27 -0700

    rps_strings were in wrong order.. still not right

--- cs100a/module2/0loops.py
+++ cs100a/module2/0loops.py
@@ -9,7 +9,7 @@ player_wins = 0
 comp_wins = 0
 gameover = 0
 
-rps_strings = ["rock","scissors", "paper"]
+rps_strings = [0, "rock","paper", "scissors"]
 player_rps = 0
 comp_rps = 0
 
@@ -17,7 +17,9 @@ print("How many rounds do you want to play? Please enter any integer: ")
 rounds = int_input_function()
 
 while rounds > 0:
-    print("1. Rock, 2. Paper, or 3. Scissors?")
+    player_rps = 0
+    comp_rps = 0
+    print("\n1. Rock, 2. Paper, or 3. Scissors?")
     valid = False
     while valid == False:
         try:
@@ -28,23 +30,22 @@ while rounds > 0:
                 valid = True
         except ValueError:
             print("Invalid Input!")
-    comp_rps = random.randint(1,3)
+    comp_rps = random.randint(0,4)
     if player_rps == comp_rps:
-        print("you played "+rps_strings[player_rps]+" and computer played "+rps_strings[player_rps]+". it's a tie!")
+        print("you played "+str(rps_strings[player_rps])+" and computer played "+str(rps_strings[comp_rps])+". it's a tie!")
     elif player_rps > comp_rps:
-        print("you played "+rps_strings[player_rps]+" and computer played "+rps_strings[player_rps]+"! "+rps_strings[player_rps]+" beats "+rps_strings[player_rps]+"! you win")
+        print("you played "+str(rps_strings[player_rps])+" and computer played "+str(rps_strings[comp_rps])+"! "+str(rps_strings[player_rps])+" beats "+str(rps_strings[comp_rps])+"! you win")
         player_wins += 1
     elif player_rps < comp_rps:
-        print("you played "+rps_strings[player_rps]+" and computer played "+rps_strings[player_rps]+"! "+rps_strings[player_rps]+" beats "+rps_strings[player_rps]+"! computer wins")
+        print("you played "+str(rps_strings[player_rps])+" and computer played "+str(rps_strings[comp_rps])+"! "+str(rps_strings[comp_rps])+" beats "+str(rps_strings[player_rps])+"! computer wins")
         comp_wins += 1
-    player_rps = 0
-    comp_rps = 0
     print("player wins: "+str(player_wins)+"   computer wins: "+str(comp_wins))
     if player_wins == 7:
         gameover = 1
     if comp_wins == 7:
         gameover = 1
-    rounds -= 1
+    if player_rps != comp_rps:
+        rounds -= 1
     
 #dictionary helps you check max between two variables!!
 dict = {player_wins:"player",comp_wins:"computer"}
