fc563bd705784a4fa5d5a402b5b4cc7b6843d4bd
Rebecca Soderberg <vogindex@gmail.com>
2023-10-04 18:45:05 -0700

    tuples!?!?!?!

--- cs100a/module1/0tuples.py
+++ cs100a/module1/0tuples.py
@@ -1,33 +1,61 @@
+#input functions
+def int_input_function():
+    valid = False
+    while valid == False:
+        try:
+            x = int(input())
+            if not x:
+                raise ValueError('empty string')
+            valid = True
+        except ValueError:
+            print("Invalid Input!")
+    return x  
+
+def float_input_function():
+    valid = False
+    while valid == False:
+        try:
+            x = float(input())
+            if not x:
+                raise ValueError('empty string')
+            valid = True
+        except ValueError:
+            print("Invalid Input!")
+    return x  
+
+
 #making list
 grocery_list = []
 #initializing item
-#this is totally the wrong way to do it, but otherwise it was complaining about it!!!
-item=[0, 1, 2]
+item=[]
 item_name=""
 #add items to list (except none)
 while item_name != "none":
     item_name = input("enter a grocery item, or none to exit: ")
     if item_name!= "none":
-        #oh, my types inputs aren't safe from breakage, are they?
-        item[0] = item_name
-        price = float(input(f"How much does the {item_name} cost? "))
-        item[1] = price
-        quantity = int(input(f"How many {item_name} are you buying? "))
-        item[2] = quantity
+        item.append(item_name)
+        print(f"How much does the {item_name} cost? ")
+        price = float_input_function()
+        item.append(price)
+        print(f"How many {item_name} are you buying? ")
+        quantity = int_input_function()
+        item.append(quantity)
     grocery_list.append(item)
-#i was trying to remove none, idk, how to i reference a tuple with a specific name?
+#still adding item twice???????????? and like, it's doing multiples of a list for the whole shopping list?
 
 print("here's your grocery list:")
 print(grocery_list)
-#for some reason it just printed eggs 3 times!!!! 
 
 
 #remove items from list if they are bought... until none remain
 print("now go to the grocery store")
 while len(grocery_list) > 0:
-    item = input("what did you put in your cart?")
-    if item in grocery_list:
+    item_name = input("what did you put in your cart?")
+    #really don't know how to do this!!!!!!!!!!!!!!
+    if item_name in item in grocery_list:
         grocery_list.remove(item)
+    print(grocery_list)
+
 
 
     
