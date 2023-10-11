850e9de029f4ce23e6a93e2334a1a59ec8cd9a33
Rebecca Soderberg <vogindex@gmail.com>
2023-10-10 21:32:10 -0700

    conclusion: idk what i am doing with directories

--- cs100a/module3/0tuples.py
+++ cs100a/module3/0tuples.py
@@ -4,32 +4,34 @@ sys.path.append("..\..")
 
 from i import int_input_function, float_input_function
 
+def create_list():
+    #making list
+    grocery_list = []
+    #initializing item
+    #add items to list (except none)
+    item_name = ""
+    #i had to put grocery_list.append(item) under if item_name != none!!!!
+    while item_name != "none":
+        item_name = input("enter a grocery item, or none to exit: ")
+        if item_name!= "none":
+            print(f"How much does the {item_name} cost? ")
+            price = float_input_function()
+            print(f"How many {item_name} are you buying? ")
+            quantity = int_input_function()
+            item = (item_name, price, quantity)
+            grocery_list.append(item)
 
-#making list
-grocery_list = []
-#initializing item
-#add items to list (except none)
-item_name = ""
-#i had to put groceryy_list.append(item) under if item_name != none!!!!
-while item_name != "none":
-    item_name = input("enter a grocery item, or none to exit: ")
-    if item_name!= "none":
-        print(f"How much does the {item_name} cost? ")
-        price = float_input_function()
-        print(f"How many {item_name} are you buying? ")
-        quantity = int_input_function()
-        item = (item_name, price, quantity)
-        grocery_list.append(item)
+    print("YOUR GROCERY LIST:")
+    #oh, this weird little thing, %.2f! like, what is that???
+    for item in grocery_list:
+        (n, p, q) = item
+        price = ("%.2f" % p)
+        print(f"{q}x {n} at ${price} each")
+    #ohhhh, you have to copy lists!!!
+    full_grocery_list = grocery_list.copy()
+    return grocery_list
 
-print("YOUR GROCERY LIST:")
-#oh, this weird little thing, %.2f! like, what is that???
-for item in grocery_list:
-    (n, p, q) = item
-    price = ("%.2f" % p)
-    print(f"{q}x {n} at ${price} each")
-#ohhhh, you have to copy lists!!!
-full_grocery_list = grocery_list.copy()
-
+grocery_list = create_list()
 
 #remove items from list if they are bought... until none remain
 print("now go to the grocery store")
---
+++ cs100a/module5/0reading.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append('..')
+
+from module3 import create_list
+
+#grocery list confirmation is reinvogorated by file import combo
+grocery_list = create_list()
+#initialize lineno
+lineno = 0
+yes = ""
+#loop through lines in text opened through file variable
+for line in grocery_list:
+    #strip lineno
+    if lineno < 4:
+        print(str(lineno)+": "+line.strip())
+    elif lineno >= 4:
+        print(line.strip())
+    #question user about purchase decision
+    if lineno < 4:
+        while yes != 'y' or yes != 'Y':
+            print("would you REALLY like to make this purchase?")
+            #accept input
+            yes = input()
+    #advance lineno
+    lineno = lineno + 1
+
+grocery_list.close()
+
+
+
