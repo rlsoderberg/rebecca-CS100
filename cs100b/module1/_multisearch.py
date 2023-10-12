223f1a4c79ec57b7e2e411909443d31d6efb9e28
Rebecca Soderberg <vogindex@gmail.com>
2023-10-12 13:07:29 -0700

    omg u don't need position? still not quite working

--- cs100b/module1/_multisearch.py
+++ cs100b/module1/_multisearch.py
@@ -9,22 +9,18 @@ def sequentialsearch(haystack, searchterm): #forgot arguments
     position = 0
     for i in haystack: #i think this is supposed to be (0, len(haystack)), not sure why yet
         if(i == searchterm): #forgot parentheses
-            item = (i, position)
-            return item
-        position += 1
+            return i
 
-    item = (-1, position)  
-    return item  
+    return -1  
 
 searchterm = input("enter a number between 1 and 10: ")
 haystack = [3,5,6,7,2,4,9]
 
-tuple = sequentialsearch(haystack, searchterm)
-(i, position) = tuple
+position = sequentialsearch(haystack, searchterm)
 
-while i == -1:
-    if i== -1:
-        print(f"{searchterm} is not in the haystack")
-    else:
-        print(f"i found {searchterm} at position {position}.")
+
+if position == -1:
+    print(f"{searchterm} is not in the haystack")
+else:
+    print(f"i found {searchterm} at position {position}.")
 
