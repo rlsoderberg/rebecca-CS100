7bda0f8439bf8c2b1dc0f7380cb9cfbbca170c4d
Rebecca Soderberg <vogindex@gmail.com>
2023-10-11 19:38:00 -0700

    there, is that working?

--- cs100a/module1/o08name.py
+++
@@ -1,5 +0,0 @@
-#request user input
-name = input("What is your name? ")
-
-#display result
-print("Your name is ",name,".")
--- cs100a/module1/o09madlib.py
+++
@@ -1,8 +0,0 @@
-#request user input
-adjective = input("Adjective: ")
-noun1 = input("Noun: ")
-verb = input("Past-tense verb: ")
-noun2 = input("Another noun: ")
-
-#display result
-print("The quick, ",adjective,noun1,verb,"over the lazy ",noun2,".")
