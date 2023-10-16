37f5e10bdafb24b3adc5c222bb923f50aa70a03e
Rebecca Soderberg <vogindex@gmail.com>
2023-10-16 14:43:11 -0700

    one restart to rule them all

---
+++ cs100a/module1/helloworld.py
@@ -0,0 +1,13 @@
+#this had better be the last time i restart!!!!!
+#i do feel like i learn more trying to think of it by myself
+#is this really necessary??? can't i just go out there and program and pick it up gradually that way???
+#noooo, why am i so preoccupied with my learning being complete
+#this is really actually pretty awful
+#hopefully it gets faster and faster though
+#oh, at the same time, i should probably practice typing properly
+#ugh, i'm really not very into it though
+#oh well here we go fine fine fine
+#and don't expect me to get much done tonight because it is orchestra night!!!
+print('Hello world.')
+#i am already regretting this. i mean, why do i have to consume my time like this??? i need to get a job, ok???
+#nOoOoOoO! i already renamed everything! whyyyy
