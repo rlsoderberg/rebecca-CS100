55e7301366b44a81fb36f27e9861adca32180ebc
Rebecca Soderberg <vogindex@gmail.com>
2023-10-15 18:47:59 -0700

    newmath program is problems. list of objects???

--- cs100b/module2/newmath.py
+++ cs100b/module2/newmath.py
@@ -1,34 +1,49 @@
 #new math. hasn't it been long enough??? we're putting in classes...
 
 class Student:
-    def __init__(self, name="", id="", birthdate="", grade_level=""):
+    def __init__(self, name="", id="", birthdate="", grade_lvl=""):
         self.name=name
         self.id=id
         self.birthdate=birthdate
-        self.grade_level=grade_level
+        self.grade_lvl=grade_lvl
 
 
-#i'm totally not doing this right!!!
 class Quiz_Question:
-    def __init__(self, question_text = "", answer="", appropriate_gradelevel=""):
-        self.question_text = question_text
+    def __init__(self, question = "", answer="", grade_lvl="", user_ans = ""):
+        self.question = question
         self.answer = answer
-        self.appropriate_gradelevel = appropriate_gradelevel
+        self.grade_lvl = grade_lvl
+        self.user_ans = user_ans
 
-    def print_question(rando):
-        print(f"\n{x + 1} :  {equation_strings[rando]}")
+    def get_question(self):
+        print("Input a math problem:")
+        self.question_text = input_function()
 
-    def get_ans():
-        user_ans = 0
+    def get_answer(self):
+        print(f"Input the answer to {self.question}:")
+        self.answer = input_function()
+
+    def get_grade_lvl(self):
+        print(f'Enter the grade level for the math problem {self.question}:')
+        self.gradelvl = input_function()
+
+    def get_user_ans(self):
+        self.user_ans = 0
+        print(f'Problem: {self.question}')
         print("Your Answer:")
-        user_ans = int_input_function()
+        self.user_ans = input_function()
 
-    def correct_ans():
-        correct_ans = answers[rando]
-    
-
-
-
+    def check_answer(self):
+        if int(self.user_ans) == self.answer:
+            print("correct")
+            this_correct = 1
+        elif int(self.user_ans) != self.answer:
+            print("\n\nincorrect\n")
+            input("Press any key to see the correct answer.\n")
+            print("\n\n\n\nCorrect Answer:")
+            print(f"{self.question}")
+            print(f"x = {self.answer}\n")
+        return this_correct
         
 import random
 
@@ -37,21 +52,30 @@ sys.path.append("../..")
                 
 from i import input_function
 
-#input function
-#valid integers only
+#auto generating some problems
+def auto_generate_problems():
+    problem_list = []
+    #theremust be a better way to put all of these into a list. 
+    problem_list.append(Quiz_Question("5x + 3 = 13", "2", "1", ""))
+    problem_list.append(Quiz_Question("14 - 3x = 2", "4", "1", ""))
+    problem_list.append(Quiz_Question("9 - 2x = 3", "3", "1", ""))
+    problem_list.append(Quiz_Question("8x + 2 = 50", "6", "1", ""))
+    problem_list.append(Quiz_Question("2x + 16 = 40", "12", "1", ""))
+    problem_list.append(Quiz_Question("12 - 2x = 2", "5", "1", ""))
+    problem_list.append(Quiz_Question("7 = 49 - 6x", "7", "1", ""))
+    problem_list.append(Quiz_Question("30 + 7x = 86", "8", "1", ""))
+    problem_list.append(Quiz_Question("4x + 20 = 56", "9", "1", ""))
+    problem_list.append(Quiz_Question("3x - 3 = 39", "14", "1", ""))
+    return problem_list
 
-def int_input_function():
-    valid = False
-    while valid == False:
-        try:
-            x = int(input(f"x = "))
-            if not x:
-                raise ValueError('empty string')
-            valid = True
-        except ValueError:
-            print("Invalid Input!")
-    return x  
+def rando_generate(rando_seq):
+    rando = random.randint(0,9)
 
+    while rando in rando_seq:
+        rando = random.randint(0,9)
+    rando_seq.append(rando)
+
+#this seems not entirely necessary, but, you know, it works
 def ready_input_function():
     ready = ""
     while ready != "Y" and ready != "N":
@@ -64,79 +88,49 @@ def ready_input_function():
             print("Invalid Input!")
     return ready  
 
+def welcome():
+    name = ""
+    ready = ""
+    print("what is your name? ")
+    name = input_function()
+    print(f"welcome, {name}, to the Tutorializer.\n")
+    print("Are you ready to be Tutorialized? Y/N")
+    ready = ready_input_function()
+    if ready == "Y":
+        print("Let's go!\n\n\n")
+    elif ready == "N":
+        print("Too bad! You cannot escape the Tutorializer!")
 
-def math_problem_generate(prob_seq):
-    rando = random.randint(0,9)
+#sequence of random, nonrepeating integers 0 - 9 for random problem order
+rando_seq = []
+rando_generate(rando_seq)
 
-    while rando in prob_seq:
-        rando = random.randint(0,9)
-    prob_seq.append(rando)
+#auto generate problem list
+problem_list = auto_generate_problems()
 
+#intro text
+welcome()
 
-#random number, and array of random numbers, to randomize equation order
-rando = 0
-prob_seq = []
-rand_list = []
-user_ans_list = []
-correct_list = []
-correct_ans_list = []
-#array of equation strings
-equation_strings = ["5x + 3 = 13", "14 - 3x = 2", "9 - 2x = 3", "8x + 2 = 50", "2x + 16 = 40", 
-                    "12 - 2x = 2", "7 = 49 - 6x", "30 + 7x = 86", "4x + 20 = 56", "3x - 3 = 39"]
-correct = 0
-#array of answer ints
-answers = [2, 4, 3, 6, 12, 5, 7, 8, 9, 14]
-
-name = ""
-ready = ""
-print("what is your name? ")
-name = input_function()
-print(f"welcome, {name}, to the Tutorializer.\n")
-print("Are you ready to be Tutorialized? Y/N")
-ready = ready_input_function()
-if ready == "Y":
-    print("Let's go!\n\n\n")
-elif ready == "N":
-    print("Too bad! You cannot escape the Tutorializer!")
-
-#well, here, let me at least get this working...
 
 correct = 0
 
 
 for x in range (0, 10):
-    math_problem_generate(prob_seq)
-    #oh boy if i want to come up with grade level i'll have to convert these to tuples won't i???
-    q = Quiz_Question(question_text = equation_strings[rando], answer=answers[rando], appropriate_gradelevel="")
-    #it's complaining about get_ans argument mismatch????
-    user_ans = q.get_ans()
-    if int(user_ans) == q.correct_ans:
-        print("correct")
-        correct += 1
-    elif int(user_ans) != q.correct_ans:
-        print("\n\nincorrect\n")
-        input("Press any key to see the correct answer.\n")
-        print("\n\n\n\nCorrect Answer:")
-        print(f"{equation_strings[rando]}")
-        print(f"x = {answers[rando]}\n")
-    
+    #i'm trying to
+    #1. choose item x in my list of randomized, nonrepeating integers
+    this_rando = rando_seq[x]
+    #2. choose the problem that corresponds to the value of this_rando in my ordered problem list
+    this_question = problem_list[this_rando]
+    #3. execute the get_user_ans behavior for the given problem object? i don't think i'm doing this right?
+    this_question.get_answer()
+
+    #also trying to check_user_ans
+    this_correct = this_question.check_answer()
+    correct += this_correct
+
 
 print(f"\n\nCongratulations! You have answered {correct} math problems correctly!")
 print(f"Your Tutorialization Level is {correct*10}%.\n")
-
-#THIS DEFINITELY DOESN'T SEEM LIKE THE CORRECT WAY TO ARRANGE THE DATA
-for x in rand_list:
-    print(f"{equation_strings[rand_list[x]]}")
-    print("Your Answer: ")
-    print(f"x = {user_ans_list[x]}")
-    if correct_list[x] == 1:
-        print("Correct!")
-    elif correct_list[x] == 0:
-        print("Incorrect!\n\n")
-        print(f"{equation_strings[rand_list[x]]}")
-        print("Correct Answer: ")
-        print(f"x = {correct_ans_list[x]}")
-    x += 1
 
 
 
