bf2bc4b5ab1df5a8b3f448aa530f2c1d203dd4c1
Rebecca Soderberg <vogindex@gmail.com>
2023-10-17 12:57:01 -0700

    a little more on those first few problems

--- cs100a/module1/variables2.py
+++ cs100a/module1/variables2.py
@@ -1,9 +1,17 @@
 #i'm doing the same thing for the 3rd problem of variables & userinput
+#right, i'm also doing the second part of this problem
+print('enter your game scores.')
+scores = []
+loopnumber = 1
+exit = 0
+while exit != "d":
+    exit = input(f"enter score {loopnumber}, or enter d if done: ")
+    if exit != "d":
+        scores.append(int(exit))
+    loopnumber += 1
 
-scores = [13, 18, 21, 17, 31]
+#scores = [13, 18, 21, 17, 31]
 
 #ooh, i'm trying to think if there is a good way to dynamically assign variable names in python
-#because if there was, i would go
-
-for x in range(0, 4):
-    <game'x'> = scores[x] #or whatever
+#what do you mean, store each of those point totals in separate variables (e.g. game1, game2, ...)?
+#is there some good way to do that???
