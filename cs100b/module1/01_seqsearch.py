6fe1e0c1290788818717ca75f70562f035bfdcac
Rebecca Soderberg <vogindex@gmail.com>
2023-10-21 17:27:47 -0700

    binary search very finnicky, trying my patience!!

---
+++ cs100b/module1/01_seqsearch.py
@@ -0,0 +1,58 @@
+#import random to generate randomized list
+import random
+
+#define selection search function that uses for loop to go through list to search
+def sequential_search(needle, haystack):
+    #for loop going through haystack and checking for needle at each location
+    #len(haystack) - 1 is end of range, since haystack starts at 0
+    print(len(haystack))
+    #remember to include the word 'range', otherwise your program will just look at haystack[0] and haystack[-1]
+    for x in range (0, len(haystack) - 1):
+        if str(haystack[x]) == str(needle):
+            return x
+    #if loop has completed without finding needle, then return -1
+    return -1
+
+
+#define function to generate random letter string
+def random_string():
+    #generate string containing all letters in alphabet
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    #initialize string to put random letters in
+    random_string = ''
+    #initialize random character
+    char = ''
+    #for loop to generate a string of 20 random characters
+    for x in range (0, 26):
+        rando = random.randrange(len(alphabet))
+        char = alphabet[rando]
+        random_string = random_string + char
+    return random_string
+
+#define main function
+def main():
+    #initialize random string
+    rand_string = ''
+    #generate random letter string
+    rand_string = random_string()
+    print(f'random letter string: {rand_string}')
+
+    #set search term
+    search_term = 'q'
+
+    #initialize location
+    location = 0
+
+    #execute selection search
+    location = sequential_search(search_term, rand_string)
+
+    if location == -1:
+        print(f"couldn't find search term {search_term} in random letter string")
+    else:
+        print(f"found search term {search_term} at location {location}.")
+
+
+main()
+
+
+
---
+++ cs100b/module1/02_binarysearch.py
@@ -0,0 +1,57 @@
+import random
+
+#define function to generate random letter string
+def number_list():
+    list = []
+    for x in range (0, 50):
+        r = random.randrange(100)
+        list.append(r)
+    return list
+
+def binary_search(haystack, needle):
+    #keep track of begin and end as numbers, not as values on list
+    begin = 0
+    end = len(haystack)
+
+    #while begin <= end keeps the search going as the searched area gets smaller and smaller
+    while begin <= end:
+        #flexible haystack length, with mobile midpoint & begin & end
+        #use floor division to get an integer midpoint, which you can use as a list position
+        midpoint = begin + (end - begin)//2
+        #cut flexible length in half each time, setting midpoint as edge
+        if haystack[midpoint] < needle:
+            begin = midpoint + 1
+        elif haystack[midpoint] > needle:
+            end = midpoint - 1
+        else:
+        #binary search narrows all the way down to midpoint = needle, still faster than going one by one 
+            return midpoint
+
+    #if loop has completed without finding needle, then return -1
+    return -1
+
+
+list = number_list()
+list.sort()
+print(f'sorted random string: {list}')
+search_term = 59
+
+location = binary_search(list, search_term)
+if location == -1:
+    print(f'did not find {search_term}')
+else:
+    print(f'found {search_term} at location {location}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
