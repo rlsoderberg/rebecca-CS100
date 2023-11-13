819339eb8f48f1dcfc48ac499c74f3d3e3a4118a
Rebecca Soderberg <vogindex@gmail.com>
2023-11-13 15:38:39 -0800

    started module 2

---
+++ cs100c/module2/01sqlcheckpoint.py
@@ -0,0 +1,17 @@
+#well, look, i have to make some kind of file! i need to sleep on this hard!!!
+#so i got the file to kind of work by converting it to ansi with notepad++
+#anyway, so now, it's complaining about my syntax
+#and why do people always write it in multiple lines? am i doing this right???
+#well, i got to a point where it returned an empty set and now idk
+#SELECT * FROM Customers WHERE 'Job Title' = 'Owner';
+
+#reminder on how to get there, in case you forget
+#cd bin
+#mariadb -u root -p
+#source C:\database\change.sql
+
+#now, looking at the file opening... there are a LOT of errors, so that might be part of the problem
+#idk tho. can i ask about this?
+
+
+#also i need to figure out how to exit partially instead of using ctrl+c
