98b7ca47721ee7a9896c3f106b3c433e598eef26
Rebecca Soderberg <vogindex@gmail.com>
2023-11-15 12:22:44 -0800

    what happened to funky o'donnell?

--- cs100c/module2/01sqlcheckpoint.py
+++ cs100c/module2/01sqlcheckpoint.py
@@ -17,7 +17,7 @@
 #oh, i got it to work with student
 #apparently the arrows are it just asking for a semicolon, so that's kind of nice
 
-#haha, this is what I get when I try to select first name from customers
+#haha, this is what I get when I try to select 'first name' from customers
 
 """
 +------------+
@@ -43,4 +43,41 @@
 +------------+
 """
 
+#also, how do you deal with... things that have spaces in the names?
+#you use... bacticks!!! for things like table names & attributes!!!
+#you also use double quotes for quotations in sql
 
+#checkpoint
+
+#1. look at this, i had to select distinct
+#select distinct `first name` from customers where `job title` = 'owner';
+"""
++---------------+
+| first name    |
++---------------+
+| Anna          |
+| Antonio       |
+| Martin        |
+| Ming-Yang     |
+| Jean Philippe |
+| Jonas         |
++---------------+
+"""
+#2. select distinct `first name`, `last name`, `company` from customers where `job title` = 'owner';
+#something's going on with my files!!! look, i've still got funky o'donnell in there somewhere!!!
+"""
++---------------+------------------+-----------+
+| first name    | last name        | company   |
++---------------+------------------+-----------+
+| Anna          | Bedecs           | Company A |
+| Antonio       | Gratacos Solsona | Company B |
+| Martin        | OÆDonnell        | Company E |
+| Martin        | O'Donnell        | Company E |
+| Ming-Yang     | Xie              | Company G |
+| Jean Philippe | Bagel            | Company Q |
+| Jonas         | Hasselberg       | Company X |
++---------------+------------------+-----------+
+"""
+#i don't get it!!! i only see this o'donnell guy once in the file i am using
+#am i going to have to come in and ask about this again???
+#i swear, this sql stuff, like, i have no idea how even to ask the internet about it!!!
