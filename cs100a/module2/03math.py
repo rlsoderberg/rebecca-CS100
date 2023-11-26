4472522f9003403e7c1721b759c2cf95a7e01b68
Rebecca Soderberg <vogindex@gmail.com>
2023-11-25 20:33:34 -0800

    what a mess, but it seems to be working? sleeps

--- cs100a/module2/03math.py
+++ cs100a/module2/03math.py
@@ -1,12 +1,6 @@
 #import random for math
 import random
 
-#ask user for their name
-name = input('please enter your name: ')
-
-#initialize correct
-correct = 0
-
 #define function to pick random operation symbol
 def randop():
     #list of operation symbols
@@ -16,10 +10,21 @@ def randop():
 
     return symb
 
+#lists where we store x, y, and symb
+xlist = []
+ylist = []
+symblist = []
+
+
 #define equation for contents of problem loop
-def probloop(correct):
-    #generate random operation symbol
-    symb = randop()
+def probloop(loopcount, newcorrect):
+    
+    if newcorrect == 0:
+        #generate random operation symbol
+        symb = randop()
+        symblist.append(symb)
+    if newcorrect == 1:
+        symb = symblist[-1]
 
     #oh cool, you could use lambda to choose operations!
     #now, i don't REALLY know how to do this, i just stole this from some person on the internet
@@ -28,10 +33,17 @@ def probloop(correct):
           '*': lambda x, y: x * y
           }
 
-
-    #choose variables
-    x = random.randrange(1, 6)
-    y = random.randrange(1, 6)
+    #depending on whether you've run the loop before... maybe generating & storing? maybe taking from list?
+    if newcorrect == 0:
+        x = random.randrange(1, 6)
+        xlist.append(x)
+    if newcorrect == 1:
+        x = xlist[-1]
+    if newcorrect == 0:
+        y = random.randrange(1, 6)
+        ylist.append(y)
+    if newcorrect == 1:
+        y = ylist[-1]
 
     #solve equation
     compans = op[str(symb)](x,y)
@@ -47,24 +59,39 @@ def probloop(correct):
         except ValueError:
             print('invalid input')
 
+    #initialize newcorrect
+    newcorrect = 0
+
     #display results; modify correct count
     if userans == compans:
         print(f'user answer({userans}) is correct.')
-        correct = 1
+        newcorrect = 1
     elif userans != compans:
-        print(f'user answer({userans}) is incorrect. correct answer is {compans}.')
+        print(f'user answer({userans}) is incorrect. try again!')
 
-    return correct
+    return newcorrect
 
-#loop to generate 10 random math equations
-for loopcount in range(0, 10):
-    newcorrect = probloop(correct)
-    correct = correct + newcorrect
+#define main
+def main():
+    #ask user for their name
+    name = input('please enter your name: ')
 
+    #initialize correct
+    correct = 0
 
-#print overall results
-print(f'\n{name} answered {correct} out of 10 questions correctly.\n')
+    #loop to generate 10 random math equations
+    for loopcount in range(0, 10):
+        newcorrect = 0
+        newcorrect = probloop(loopcount, newcorrect)
+        correct = correct + newcorrect
+        while newcorrect == 0:
+            newcorrect = 1
+            newcorrect = probloop(loopcount, newcorrect)
 
+    #print overall results
+    print(f'\n{name} answered {correct} out of 10 questions correctly.\n')
+
+main()
 
     
     
