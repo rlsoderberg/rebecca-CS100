81c55f9bd52df7be4b9fc9afd1ad2af47317e91d
Rebecca Soderberg <vogindex@gmail.com>
2023-11-27 20:59:03 -0800

    wElL.... i've got 3 suspects

--- cs100a/module3/01lists.py
+++ cs100a/module3/01lists.py
@@ -4,9 +4,36 @@ import random
 team = []
 
 #list of player numbers
-numlist = [0, 0, 0, 0]
+numlist = [-2]
 #i don't know why it seemed to want more numbers
 
+# ask the user for names.
+# enter 'done' when complete
+player = ''
+number = -1
+while player != 'done':
+    player = input('give me a player name: ')
+    if player != 'done':
+        number = random.randrange(99)
+        team.append((str(player), str(number)))
+#well... i'm going back to a version that works
+#it didn't like me preventing duplicates!!!
+
+sorted_by_num = sorted(team, key=lambda tup: tup[1])
+
+print('your team is:')
+#right, so i was trying to figure out why it was complaining about me 'subscripting' p
+#so i was like, well, why don't i do for p in team?
+#and then it's like oh yeah, i wanted something that was reversable
+#so idk if the loop is the issue, or my lambda, or my tuple declaration, but those are my 3 suspects 
+for p in range(0, len(sorted_by_num)):
+    print(p[0] + p[1])
+
+print('your team is:')
+for p in reversed(range(0, len(sorted_by_num))):
+    print(p[0] + p[1])
+        
+"""
 #print instructions
 print("please enter names of team members; type 'done' when done.")
 #request name input for each player
@@ -18,7 +45,7 @@ while player != 'done':
     number = -1
     while number not in numlist:
         #it keeps getting stuck on this!!!!!!!
-        number = random.randrange(1, 99)
+        number = random.randrange(99)
     #appending tuple to list requires two parentheses
     team.append((player, number))
 
@@ -33,7 +60,7 @@ print('your team is:')
 for p in range(0, len(sorted_by_num)):
     print('\t' + p)
 
-"""
+
 print('your team is:')
 for p in reversed(range(0, len(sorted_by_num))):
     print('\t' + p)
