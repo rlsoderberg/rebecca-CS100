c2173bca0d2b5fcb82bd54c4d83e7e1312641817
Rebecca Soderberg <vogindex@gmail.com>
2023-11-28 12:45:04 -0800

    vscode gitlab acting off. better save b4 relogging

--- cs100a/module3/01lists.py
+++ cs100a/module3/01lists.py
@@ -1,6 +1,13 @@
 import random
 
-#create team list
+#1.
+
+#ask for user's name
+name = input("hi! you must be my new basketball coaching assistant! what's your name? ")
+print("first you're going to fill out our team roster.")
+
+ 
+# #create team list
 team = []
 
 #list of player numbers
@@ -11,7 +18,7 @@ numlist = []
 player = ''
 number = -1
 while player != 'done':
-    player = input('give me a player name: ')
+    player = input('name of player 1: ')
     if player != 'done':
     #oh, it got stuck because it didn't like my 'while number not in numlist' loop!!!
         team.append(player)
@@ -40,7 +47,45 @@ reverse = sorted(tupleteam, key=lambda tup: -tup[1])
 print('your team in reverse:')
 print(reverse)
 
-        
+#2. (continuation)
+
+#see if user name is in list of names
+if name in tupleteam:
+    print("I can't wait to play")
+else:
+    print("Put me in coach, I'm ready to play.")
+
+#3.
+
+#grocery list
+glist = []
+
+#initialize item
+item = ''
+
+#loop to get items on list until user is finished
+while item != "none":
+    #request user to input grocery item
+    item = input("enter a grocery item, or none to exit: ")
+    glist.append(item)
+    #this seems overly complicated! #i swear, i had a simpler version of this that was working before...
+    if item == "none":
+        glist.pop(len(glist) - 1)
+
+
+print(glist)
+print('now go to the grocery store')
+
+#loop until list is empty and cart is full
+while len(glist) > 0:
+    tocart = input('what did you put in your cart? ')
+    #i just wasted a bunch of mental energy because i added this to 01lists but i forgot to add it to 00tests!
+    #remember, the goal is always 0
+    if tocart in glist:
+        glist.remove(tocart)
+
+print('you can check out now.')
+
 
 
 
