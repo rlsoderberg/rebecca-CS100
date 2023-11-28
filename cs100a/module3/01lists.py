c50536c450534133d6443b1c6857f9990b8af5e9
Rebecca Soderberg <vogindex@gmail.com>
2023-11-28 06:39:03 -0800

    i still want to get a while loop in...

--- cs100a/module3/01lists.py
+++ cs100a/module3/01lists.py
@@ -4,8 +4,7 @@ import random
 team = []
 
 #list of player numbers
-numlist = [-2]
-#i don't know why it seemed to want more numbers
+numlist = []
 
 # ask the user for names.
 # enter 'done' when complete
@@ -13,63 +12,41 @@ player = ''
 number = -1
 while player != 'done':
     player = input('give me a player name: ')
-    if player != 'done' and number not in numlist:
-    #oh, it just didn't like my while loop!!!
+    if player != 'done':
+    #oh, it got stuck because it didn't like my 'while number not in numlist' loop!!!
+        team.append(player)
+
+#
+#i'm going to try zip, to make a list of tuples, and make a whole other thing for the numbers
+for p in team:
+    number = random.randrange(99)
+    if number not in numlist:
+        numlist.append(number)
+    #here's what i wish i could use, but it gets stuck: while number in numlist:
+    else:
         number = random.randrange(99)
-        team.append((player, number))
-#well... i'm going back to a version that works
-#it didn't like me preventing duplicates!!!
+        numlist.append(number)
+    #see, this only gives me two chances at non-duplicacy, where i really want to use a while loop
+    #but it doesn't seem to like when i use while loops!!!
+    
+#zip up names with numbers
+tupleteam = list(zip(team, numlist))
 
-sorted_by_num = sorted(team, key=lambda tup: tup[1])
-
-print('your team is:')
-#right, so i was trying to figure out why it was complaining about me 'subscripting' p
-#so i was like, well, why don't i do for p in team?
-#and then it's like oh yeah, i wanted something that was reversable
-#so idk if the loop is the issue, or my lambda, or my tuple declaration, but those are my 3 suspects 
-
-#well, ok, i went back to basics, and i am just printing from an unsorted list
-#it gives me a weird output, but no errors
-#like wat tho? i thought i clearly gave it the name strings
-for p in range(0, len(team)):
-    print(p)
+#sort by numbers
+sorted_by_num = sorted(tupleteam, key=lambda tup: tup[1])
 
 print('your team is:')
-for p in reversed(range(0, len(team))):
-    print(p)
+print(sorted_by_num)
+
+#sort by numbers in reverse
+reverse = sorted(tupleteam, key=lambda tup: -tup[1])
+
+print('your team in reverse:')
+print(reverse)
+
         
-"""
-#print instructions
-print("please enter names of team members; type 'done' when done.")
-#request name input for each player
-player = ''
-while player != 'done':
-    player = input('give me a player name: ')
-    #you know what, i don't understand why this 'if player != done' is here!!! i'm going to try it without!!!
-    #i tried putting it back in, and it did NOT help with my mystery error...
-    number = -1
-    while number not in numlist:
-        #it keeps getting stuck on this!!!!!!!
-        number = random.randrange(99)
-    #appending tuple to list requires two parentheses
-    team.append((player, number))
-
-#oh, here's lambda again!
-#i swear, when i first used it for math tutor in lesson 1, i saw it in the preview for the next week
-#but we never went over it!!
-#now lambda is something i really SHOULD ask about
-#because look, here i am once again stealing it from some person on the internet
-sorted_by_num = sorted(team, key=lambda tup: tup[1])
-
-print('your team is:')
-for p in range(0, len(sorted_by_num)):
-    print('\t' + p)
 
 
-print('your team is:')
-for p in reversed(range(0, len(sorted_by_num))):
-    print('\t' + p)
-"""
 
 
 
