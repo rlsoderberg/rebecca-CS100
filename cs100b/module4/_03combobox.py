a0ecc66cdc0d773eef9c29eb6ecb9960a1bda476
Rebecca Soderberg <vogindex@gmail.com>
2023-12-10 17:09:18 -0800

    qsize interrupting mainwindow!!!

--- cs100b/module4/_03combobox.py
+++ cs100b/module4/_03combobox.py
@@ -1,6 +1,9 @@
 #oops, i think i only went through the first section of the pyqt tutorials
 #i'll go through them all later!!! but here's the one on combo boxes, which i will use to get mine working
 
+#i'm not sure how to do get screen resolution in pyqt6!!!
+#i'm trying... qsize?
+
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QRadioButton, QLabel, QVBoxLayout, QComboBox
 from PyQt6.QtCore import Qt
@@ -37,6 +40,11 @@ class MainWindow(QWidget):
         # show the window
         self.show()
 
+        #when i try to do this, the window disappears instantaneously!!!
+        #screen_size = QSize(width(), height())
+        #should i put it up here, in mainwindow def?
+        #and i don't know how i would pass it into my new window classes, or whatever...
+
     def update(self):
         if self.cb_location.currentText() == 'Upper Left':
             self.result_label.setText('Upper Left')
@@ -47,9 +55,18 @@ class MainWindow(QWidget):
         elif self.cb_location.currentText() == 'Lower Right':
             self.result_label.setText('Lower Right')
 
+#is this REALLY the best way to do it? to make a class for each window location???
+class UL_window(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("UL Window")
+        layout.addWidget(self.label)
+        self.setLayout(layout)
 
+        self.setGeometry(50, 50, 200, 50)
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QApplication([])
     window = MainWindow()
     sys.exit(app.exec())
