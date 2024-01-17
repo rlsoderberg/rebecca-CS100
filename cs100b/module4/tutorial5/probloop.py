6ee1c66ac3c295184bfa5c689b7ac6b50929aeb6
Rebecca Soderberg <vogindex@gmail.com>
2024-01-16 16:52:25 -0800

    BOY I DON'T KNOW ABOUT THIS THING (waiting for submit)

--- cs100b/module4/tutorial5/probloop.py
+++ cs100b/module4/tutorial5/probloop.py
@@ -56,7 +56,11 @@ class mainwindow (QWidget):
         self.img3 = QPixmap('')
         self.label3.setPixmap(self.img3)
         self.imglayout.addWidget(self.label3)
-        print('so it is from here that i go to the click functions')
+
+        #i'm going to try to verify interaction starting up here...
+        #i mean, i don't know if i need this one, because i did put it in print
+        #self.interaction_marker = False
+
         self.button1 = QPushButton('')
         self.button1.clicked.connect(self.click1)
         self.sublayout.addWidget(self.button1)
@@ -75,18 +79,29 @@ class mainwindow (QWidget):
         self.button.clicked.connect(self.submit)
         self.mainlayout.addWidget(self.button)
 
-        #set the probcount up here, because what i should really be doing is writing all of this in the probloop
-        self.probtotal = 2
+        self.probtotal = 3
         self.points = 0
-        self.continueloop = False
+        self.probcount = 0
+        self.interaction_marker = False
 
         self.show()
         #wait, can i literally just say the name of the probloop after info???
         self.info()
         #putting probloop in actual loop
         #this loop still isn't working, i don't think!!! so i'm kind of cheating on it for now...
-        for self.probcount in range(0, self.probtotal):
-            self.probloop(self.probcount)
+
+        #so, um, probloop isn't where i need my interaction marker at all, is it???
+        #shouldn't i put it up with the buttons?
+
+        #um, this doesn't seem exactly right either, but at least it's starting on the first problem...
+        #i'll probably have to sleep on this, AGAIN!!!
+       
+        for self.probcount in range(1, self.probtotal):
+            print(f'probcount right before for loop body: {self.probcount}')
+            print(f'interaction_marker right before for loop body: {self.interaction_marker}')
+            if self.probcount == 1 or self.interaction_marker == True:
+                self.printing(self.probcount)
+                #it's looping (or failing to loop) before i hit submit... how do i force it to wait for submit???
         
 
     #alright, well... data??? uhhhh, i'm sure we've covered how to import from text files... so i need:
@@ -95,59 +110,50 @@ class mainwindow (QWidget):
     #C) images
     #D) a txt of correct numbers
     #E) a txt of answer options
-    def probloop(self, probcount):
-        #so what i'm trying to do... is stop it from looping through, regardless of whether the user answered...
-        #so could i put an interaction marker on incorrect & correct?
-        self.interaction_marker = False
-        
-        #uh, i'll think about how to use my interaction marker!!!
-        self.get_variables(probcount)
-        self.setTheText()
+    def printing(self, probcount):
 
         self.select = 0
+        self.interaction_marker = False
 
+        self.get_variables(probcount)
+    
+        #so, REALLY... all i should have to do is setText, right?
+        self.title.setText(str(self.probcount) + '. ' + self.thisquestion)
 
-    def setTheText(self):
-        
-            #so, REALLY... all i should have to do is setText, right?
-            self.title.setText(str(self.probcount + 1) + '. ' + self.thisquestion)
+        self.img1 = QPixmap(self.imagelist[0])
+        self.label1.setPixmap(self.img1)
 
-            self.img1 = QPixmap(self.imagelist[0])
-            self.label1.setPixmap(self.img1)
+        self.img2 = QPixmap(self.imagelist[1])
+        self.label2.setPixmap(self.img2)
 
-            self.img2 = QPixmap(self.imagelist[1])
-            self.label2.setPixmap(self.img2)
+        self.img3 = QPixmap(self.imagelist[2])
+        self.label3.setPixmap(self.img3)
 
-            self.img3 = QPixmap(self.imagelist[2])
-            self.label3.setPixmap(self.img3)
+        self.button1.setText(self.optionchoices[0])
 
-            self.button1.setText(self.optionchoices[0])
+        self.button2.setText(self.optionchoices[1])
 
-            self.button2.setText(self.optionchoices[1])
+        self.button3.setText(self.optionchoices[2])
 
-            self.button3.setText(self.optionchoices[2])
-
-            self.button.setText('Submit')
+        self.button.setText('Submit')
 
 
     def click1(self):
-        #print('magicians')
         self.button1.setStyleSheet('QPushButton {background-color: red}')
         self.button2.setStyleSheet('QPushButton {background-color: blue}')
         self.button3.setStyleSheet('QPushButton {background-color: blue}')
         self.select = 1
     def click2(self):
-        #print('villagers')
         self.button2.setStyleSheet('QPushButton {background-color: red}')
         self.button1.setStyleSheet('QPushButton {background-color: blue}')
         self.button3.setStyleSheet('QPushButton {background-color: blue}')
         self.select = 2
     def click3(self):
-        #print('demons')
         self.button3.setStyleSheet('QPushButton {background-color: red}')
         self.button1.setStyleSheet('QPushButton {background-color: blue}')
         self.button2.setStyleSheet('QPushButton {background-color: blue}')
         self.select = 3 
+
     def submit(self):
         #to keep everything from being incorrect all the time, we must put this in a separate function...
         #we also need to make sure they are the same variable type
@@ -191,11 +197,11 @@ class mainwindow (QWidget):
         self.questionlist = data.replace('\n', ' ').split(".")
         questionlist.close()
 
-        self.thisquestion = self.questionlist[probcount]
+        self.thisquestion = self.questionlist[probcount - 1]
 
         #images. now, HOW am i going to reference images within a specific subfolder?
         #i can do images up here, because i've got self.filenames!
-        dir = (f"./img/{probcount + 1}")
+        dir = (f"./img/{probcount}")
         self.dirlist = os.listdir(path=dir)
         #making an imagelist... from the dirlist...
         self.imagelist = []
@@ -209,7 +215,7 @@ class mainwindow (QWidget):
 
         data = self.correcttxt.read()
         self.correctlist = data.replace('\n', ' ').split(".")
-        self.correct = self.correctlist[probcount]
+        self.correct = self.correctlist[probcount - 1]
         self.correcttxt.close()
 
         #answer options
@@ -219,7 +225,7 @@ class mainwindow (QWidget):
                 self.optionlist = file
         
         #uh, i wasn't expecting this complicated address to work, but it might have? idk?
-        self.optiontxt = open(f'img/{probcount + 1}/{str(self.optionlist)}', "r")
+        self.optiontxt = open(f'img/{probcount}/{str(self.optionlist)}', "r")
 
         data = self.optiontxt.read()
         self.optionchoices = data.replace('\n', ' ').split(".")
