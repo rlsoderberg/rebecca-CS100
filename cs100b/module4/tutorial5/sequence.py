186b684ae9ce6cf7bb4e521ca45c7e4d8b8095a6
Rebecca Soderberg <vogindex@gmail.com>
2024-01-17 07:19:52 -0800

    i will finish this later...

--- cs100b/module4/tutorial5/sequence.py
+++ cs100b/module4/tutorial5/sequence.py
@@ -1,18 +1,13 @@
-#ok, so i thought, let me just make a simple program with what i'm actually trying to do
-#so where i'm stuck is emitting a signal when you close a message box?
-#that's why i saved the zetcode guy, because he has a bunch of stuff and i'm like, maybe i should steal this
-#so i'm going to try and use some of his code right now...
+#after sleeping on this, i thought... we really don't need a signal on close message box
+#first i thought, can't we do a signal on open message box? but that seems awkward
+#more importantly, i realized that the message box has buttons! surely those can emit a signal
+#so... with that, i think i'd also have to abandon the for loop
+#instead count with while < and incremented counter
+
 import sys
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import *
-
-#this is from zetguy. creating a whole new class? whooaaa!!!
-#honestly, the rest of it... idk. it's about closing app, which, i want closing message box, right?
-#but this is sure cool. creating a class? wow. qobject? what? cool.
-class Communicate(QObject):
-
-    closeApp = pyqtSignal()
 
 class MainWindow(QWidget):
     def __init__(self, parent=None):
@@ -21,8 +16,8 @@ class MainWindow(QWidget):
         self.setWindowTitle('QMessageBox')
         self.setGeometry(100, 100, 300, 100)
 
-        layout = QHBoxLayout()
-        self.setLayout(layout)
+        self.mainlayout = QHBoxLayout()
+        self.setLayout(self.mainlayout)
 
         #so... the important thing is the info message box & the submit message box
 
@@ -35,11 +30,14 @@ class MainWindow(QWidget):
         self.info()
     
     def info(self):
-        QMessageBox.information(
+        msgBox = QMessageBox.information(
             self,
             'Banjo Tyrwo',
             'Answer all questions\nto gain super combo.\nYou have one minute\nto answer each question.'
         )
+        #well, that's one internet thing that didn't work! 
+        #i'd like to omit the x button because how do you get a signal from that?
+        msgBox.setWindowFlags(Qt.CustomizeWindowHint | Qt.WindowTitleHint)
     def submit(self):
         if int(self.select) == int(self.correct):
             self.corrects()
