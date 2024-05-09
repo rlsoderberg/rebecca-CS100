6a65cc992eec731560fb0533639498dd062fa3f6
Rebecca Soderberg <vogindex@gmail.com>
2024-05-08 21:39:23 -0700

    i literally still do not know why it never explodes

--- cs100d/module1/some.js
+++ cs100d/module1/some.js
@@ -1,7 +1,13 @@
 function changeImage() {
-    if (document.getElementById("skel-img").src == "skel_dance.gif"){
-        document.getElementById("skel-img").src = "skel_explode.gif";
+
+    var skel_img = document.getElementById("skel_img");
+    
+    var youtubeimgsrc = document.getElementById("skel_img").src;
+    document.getElementById("demo").innerHTML = youtubeimgsrc;
+
+    if (skel_img.src == "skel_dance.gif"){
+        skel_img.src = "skel_explode.gif";
     } else {
-        document.getElementById("skel-img").src = "skel_dance.gif";
+        skel_img.src = "skel_dance.gif";
     }
 }
