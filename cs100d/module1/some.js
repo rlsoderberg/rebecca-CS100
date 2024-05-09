c2829b100934c6e37b347a1c754b566fc51ff1b4
Rebecca Soderberg <vogindex@gmail.com>
2024-05-09 13:02:40 -0700

    javascript demo

--- cs100d/module1/some.js
+++ cs100d/module1/some.js
@@ -1,7 +1,4 @@
-function revert(){
-    document.getElementById('skel_img').src="skel_dance.gif";
-}
-     
+//explode briefly switches gif
 function explode(){
     document.getElementById('skel_img').src="skel_explode.gif";
     setTimeout(function() {
