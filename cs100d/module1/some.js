4abb5f7eac19432fa43f491d2742cde62fce591e
Rebecca Soderberg <vogindex@gmail.com>
2024-05-09 08:45:21 -0700

    didn't change much, got it to work somehow. while messing with it, i thought, i don't see people using this onclick thing for actual whack-a-mole. we'll see

--- cs100d/module1/some.js
+++ cs100d/module1/some.js
@@ -1,13 +1,13 @@
-function changeImage() {
-
-    var skel_img = document.getElementById("skel_img");
-    
-    var youtubeimgsrc = document.getElementById("skel_img").src;
-    document.getElementById("demo").innerHTML = youtubeimgsrc;
-
-    if (skel_img.src == "skel_dance.gif"){
-        skel_img.src = "skel_explode.gif";
-    } else {
-        skel_img.src = "skel_dance.gif";
-    }
+function revert(){
+    document.getElementById('skel_img').src="skel_dance.gif";
 }
+     
+function explode(){
+    document.getElementById('skel_img').src="skel_explode.gif";
+    setTimeout(function() {
+        document.getElementById('skel_img').src="skel_dance.gif";
+      }, 400);
+}
+
+
+
