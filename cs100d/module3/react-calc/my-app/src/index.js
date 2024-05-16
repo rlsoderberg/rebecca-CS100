d398cfcb0b9bd682ca564821a601a57059250b22
Rebecca Soderberg <vogindex@gmail.com>
2024-05-16 16:32:51 -0700

    oh!!! html is in public!!! well, i thought, ooh, maybe now i can put in some of those falling digits. i was thinking about using framer motion to do some basic animations, but they use these tsx files??? i'll look around a little bit more, but i'm sure there will be more in proper tutorials or whatever. uhhh, what was the other big one i saw somewhere??? react spring??? these examples use tsx too!! oh wait, that's just typescript, which is basically javascript? so i'm totally doing this draggable list!!! no, that's hard. i at least put in this super easy little draggable thing. come on! got to have a little fun! wanted to make it interactive so you could pick different worlds, but not sure how to combine function with render???

--- cs100d/module3/react-calc/my-app/src/index.js
+++ cs100d/module3/react-calc/my-app/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import Draggable from 'react-draggable';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 
+/*
+function clicknum(num) {
+    var world = document.getElementById('world')
+    if (num == 2)
+        world.src = '../public/venus.png'
+    else if (num == 3)
+        world.src = '../public/earth.png'
+    else if (num == 4)
+        world.src = '../public/mars.png'
+}
+*/
+
+//so what's the difference between writing something in Main and writing it in the HTML???
 class Main extends React.Component{
     render () {
         //you knew we'd start with hello world, didn't you?
         return(
-            <div className = 'Main'>
-                <p>Hello World</p>
+            <Draggable>
+            <div className = 'Hello'>
+                <p class = 'HELLO'>Hello</p>
             </div>
+            </Draggable>
         )
     }
 }
+
 
 //find the element with id Root in the HTML file, and place our Main class there.
 const root = ReactDOM.createRoot(document.getElementById("root"));
