46f99a517d5f8c12f90fd8c16ba96325b13f3555
Rebecca Soderberg <vogindex@gmail.com>
2024-05-16 20:56:46 -0700

    i'm messing about trying to get it to run the function while rendering or whatever, but i'm giving up for now!!! but i'm thinking, too many copyrighted musics in the last couple modules. i am going to take out hello earth & equinoxe 5, and replace them with midi versions! that seems only proper! of course, using square wave, because i just love that chiptuney sound.

--- cs100d/module3/react-calc/my-app/src/index.js
+++ cs100d/module3/react-calc/my-app/src/index.js
@@ -3,7 +3,6 @@ import Draggable from 'react-draggable';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 
-/*
 function clicknum(num) {
     var world = document.getElementById('world')
     if (num == 2)
@@ -13,7 +12,6 @@ function clicknum(num) {
     else if (num == 4)
         world.src = '../public/mars.png'
 }
-*/
 
 //so what's the difference between writing something in Main and writing it in the HTML???
 class Main extends React.Component{
