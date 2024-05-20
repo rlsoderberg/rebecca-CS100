787984b08007d59534118549c72fa46841e98618
Rebecca Soderberg <vogindex@gmail.com>
2024-05-20 10:47:31 -0700

    oOoOoOh!!! addresses automatically start from public!!! Well, it's kind of working. It says undefined when you do more than one number. Something wrong with numberClick!!! I tried using olddisplay, but THAT didn't help... oh, I see. I toootally wrote newdisplay instead of display, oops. I am having this annoying little problem where I am trying to center my table...

--- cs100d/module3/calc/src/App.js
+++ cs100d/module3/calc/src/App.js
@@ -1,15 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="../public/react-calc.png" className="react-calc" alt="React calculator" />
-        <p>
-          Blank React Calc
-        </p>
-      </header>
     </div>
   );
 }
--- cs100d/module3/calc/src/index.js
+++ cs100d/module3/calc/src/index.js
@@ -4,14 +4,54 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Main extends React.Component{
+  constructor (){
+    super ()
+    this.state = {display: '0'}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    //const numberClick = this.numberClick.bind(this)
+
+  }
+
+  numberClick (event){
+    const {display} = this.state
+    const val = event.target.value
+    var newdisplay
+    if (display === '0') {
+      newdisplay = val
+    }
+    else
+      newdisplay = display + val
+    this.setState ({...this.state, display: newdisplay})
+  }
+
+  render() {
+    const { display } = this.state
+    const numberClick = this.numberClick.bind(this)
+    return(
+      <div class = 'center'>
+        <img src = 'calc-fav.png' class = 'calc'></img>
+        <p class = 'title'>REACT<br></br>CALC</p>
+        <p class = 'HELLO'>Hello {display}</p>
+        <div class = 'calc'>
+          <table>
+            <tr>
+              <td colspan="5"><span className="display" id="display">{display}</span></td>
+            </tr>
+            <tr>
+              <td><button className="number" onClick={numberClick} value='7'>7</button></td>
+              <td><button className="number" onClick={numberClick} value='8'>8</button></td>
+              <td><button className="number" onClick={numberClick} value='9'>9</button></td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    )
+  }
+
+  
+}
+
+//wait, what's the difference between App.js and index.js??? do i even need app.js???
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Main />)
