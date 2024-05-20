ea6fd6bacabe946c6fce94d13dbfb3d91098b222
Rebecca Soderberg <vogindex@gmail.com>
2024-05-20 12:06:55 -0700

    It's centered now, and I don't even know what the difference is!!! And now it gives undefined at the beginning. What did I do???

--- cs100d/module3/calc/src/index.js
+++ cs100d/module3/calc/src/index.js
@@ -9,39 +9,63 @@ class Main extends React.Component{
     super ()
     this.state = {display: '0'}
 
-    //const numberClick = this.numberClick.bind(this)
-
   }
 
   numberClick (event){
     const {display} = this.state
     const val = event.target.value
     var newdisplay
-    if (display === '0') {
+    if (display === '0')
       newdisplay = val
-    }
     else
       newdisplay = display + val
+    this.setState ({...this.state, display: newdisplay})
+  }
+
+  otherClick(event) {
+    const val = event.target.value
+    console.log('you have clicked something else')
+    var newdisplay
+    if(val === '0')
+      newdisplay = '0'
     this.setState ({...this.state, display: newdisplay})
   }
 
   render() {
     const { display } = this.state
     const numberClick = this.numberClick.bind(this)
+    const otherClick = this.otherClick.bind(this)
+  
     return(
       <div class = 'center'>
-        <img src = 'calc-fav.png' class = 'calc'></img>
+        <img src = 'calc-fav.png' class = 'calc-pic'></img>
         <p class = 'title'>REACT<br></br>CALC</p>
         <p class = 'HELLO'>Hello {display}</p>
-        <div class = 'calc'>
+        <div class = 'calc-box'>
           <table>
             <tr>
-              <td colspan="5"><span className="display" id="display">{display}</span></td>
+              <td><button class="number" onClick={numberClick} value='7'>7</button></td>
+              <td><button class="number" onClick={numberClick} value='8'>8</button></td>
+              <td><button class="number" onClick={numberClick} value='9'>9</button></td>
+              <td><button class="op" onclick={otherClick} value='1'>+</button></td>
             </tr>
             <tr>
-              <td><button className="number" onClick={numberClick} value='7'>7</button></td>
-              <td><button className="number" onClick={numberClick} value='8'>8</button></td>
-              <td><button className="number" onClick={numberClick} value='9'>9</button></td>
+              <td><button class="number" onClick={numberClick} value='4'>4</button></td>
+              <td><button class="number" onClick={numberClick} value='5'>5</button></td>
+              <td><button class="number" onClick={numberClick} value='6'>6</button></td>
+              <td><button class="op" onClick={otherClick} value='1'>-</button></td>
+            </tr>
+            <tr>
+              <td><button class="number" onClick={numberClick} value='1'>1</button></td>
+              <td><button class="number" onClick={numberClick} value='2'>2</button></td>
+              <td><button class="number" onClick={numberClick} value='3'>3</button></td>
+              <td><button class="op" onClick={otherClick} value='1'>*</button></td>
+            </tr>
+            <tr>
+              <td><button class="number" onClick={numberClick} value='0'>0</button></td>
+              <td><button class="number" onClick={otherClick} value='0'>CE</button></td>
+              <td><button class="op" onClick={otherClick} value='1'>=</button></td>
+              <td><button class="op" onClick={otherClick} value='1'>/</button></td>
             </tr>
           </table>
         </div>
