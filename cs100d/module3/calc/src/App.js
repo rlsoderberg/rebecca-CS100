ed659035665caaaa8bb0125e54053f08c312e18d
Rebecca Soderberg <vogindex@gmail.com>
2024-05-20 09:20:07 -0700

    I moved my files around, so that made a whole lot of ruckus. I tried to put an image on my blank React page, and it didn't work! See, I must be writing these addresses wrong! It seems like it should be simple, but nooo!!!

---
+++ cs100d/module3/calc/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="../public/react-calc.png" className="react-calc" alt="React calculator" />
+        <p>
+          Blank React Calc
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
