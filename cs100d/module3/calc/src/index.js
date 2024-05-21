27529358d5e2183d4002756fa3c73fa04a475de0
Rebecca Soderberg <vogindex@gmail.com>
2024-05-20 20:38:55 -0700

    finished up to components

--- cs100d/module3/calc/src/index.js
+++ cs100d/module3/calc/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Main extends React.Component{
   constructor (){
     super ()
-    this.state = {display: '0'}
+    this.state = {display: '0', leftnum: 0, op:''}
 
   }
 
@@ -20,17 +20,45 @@ class Main extends React.Component{
     this.setState ({...this.state, display: newdisplay})
   }
 
-  otherClick(event) {
+  ceClick(event) {
+    const {display} = this.state
     const val = event.target.value
     console.log('you have clicked something else')
     var newdisplay = '0'
     this.setState ({...this.state, display: newdisplay})
   }
 
+  opClick(event) {
+    const {display} = this.state
+    const leftnum = parseInt(display)
+    const op = event.target.value
+    this.setState({...this.state, display: '0', leftnum: leftnum, op: op})
+  }
+
+  eqClick() {
+    const {leftnum, op, display} = this.state
+    if (op !== '') {
+      const rightnum = parseInt(display)
+      var res = 0
+      if (op === '+')
+        res = leftnum + rightnum
+      else if (op === '-')
+        res = leftnum - rightnum
+      else if (op === '*')
+        res = leftnum * rightnum
+      else if (op === '/')
+        res = leftnum / rightnum
+
+      this.setState({...this.state, display:res.toString(), leftnum: 0, op: ''})
+    }
+  }
+
   render() {
     const { display } = this.state
     const numberClick = this.numberClick.bind(this)
-    const otherClick = this.otherClick.bind(this)
+    const ceClick = this.ceClick.bind(this)
+    const opClick = this.opClick.bind(this)
+    const eqClick = this.eqClick.bind(this)
   
     return(
       <div class = 'center'>
@@ -46,25 +74,25 @@ class Main extends React.Component{
               <td><button class="number" onClick={numberClick} value='7'>7</button></td>
               <td><button class="number" onClick={numberClick} value='8'>8</button></td>
               <td><button class="number" onClick={numberClick} value='9'>9</button></td>
-              <td><button class="op" onclick={otherClick}>+</button></td>
+              <td><button class="op" onClick={opClick} value = '+'>+</button></td>
             </tr>
             <tr>
               <td><button class="number" onClick={numberClick} value='4'>4</button></td>
               <td><button class="number" onClick={numberClick} value='5'>5</button></td>
               <td><button class="number" onClick={numberClick} value='6'>6</button></td>
-              <td><button class="op" onClick={otherClick}>-</button></td>
+              <td><button class="op" onClick={opClick} value = '-'>-</button></td>
             </tr>
             <tr>
               <td><button class="number" onClick={numberClick} value='1'>1</button></td>
               <td><button class="number" onClick={numberClick} value='2'>2</button></td>
               <td><button class="number" onClick={numberClick} value='3'>3</button></td>
-              <td><button class="op" onClick={otherClick}>*</button></td>
+              <td><button class="op" onClick={opClick} value = '*'>*</button></td>
             </tr>
             <tr>
               <td><button class="number" onClick={numberClick} value='0'>0</button></td>
-              <td><button class="number" onClick={otherClick}>CE</button></td>
-              <td><button class="op" onClick={otherClick}>=</button></td>
-              <td><button class="op" onClick={otherClick}>/</button></td>
+              <td><button class="number" onClick={ceClick}>CE</button></td>
+              <td><button class="op" onClick={eqClick}>=</button></td>
+              <td><button class="op" onClick={opClick} value = '/'>/</button></td>
             </tr>
           </table>
         </div>
