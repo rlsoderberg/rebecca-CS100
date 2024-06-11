52f01f304668f4b498687dbc4c8ab709d923eea8
Rebecca Soderberg <vogindex@gmail.com>
2024-06-11 15:10:36 -0700

    well, i'm just boppin along, uh, relocating a bunch of React files, i guess, and look, for some of my files, index.css & index.js in src in calc, for example, it says D for deleted next to them! And when I try to delete them and recreate them, it says D as soon as I make them with the same filename! I guess I could use different filenames. Why is it doing that though??? Anyway, I haven't even gotten to the components yet! See, what I did, was I thought, ooh, I got videos, I am going to redo all of these first React projects! So now I'm having weird problems with those, and I haven't even gotten to the components yet... honestly, having typed all of these already in the past, I might just, like, make a new module 3 folder, and try pasting everything in.

--- cs100d/module3/calc/src/index.js
+++ cs100d/module3/calc/src/index.js
@@ -1,109 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import {Button, ButtonType} from './button'
+import './calc.css';
 
-class Main extends React.Component{
-  constructor (){
-    super ()
-    this.state = {display: '0', leftnum: 0, op:''}
-
-  }
-
-  numberClick (event){
-    const {display} = this.state
-    const val = event.target.value
-    var newdisplay
-    if (display === '0')
-      newdisplay = val
-    else
-      newdisplay = display + val
-    this.setState ({...this.state, display: newdisplay})
-  }
-
-  ceClick(event) {
-    const {display} = this.state
-    const val = event.target.value
-    console.log('you have clicked something else')
-    var newdisplay = '0'
-    this.setState ({...this.state, display: newdisplay})
-  }
-
-  opClick(event) {
-    const {display} = this.state
-    const leftnum = parseInt(display)
-    const op = event.target.value
-    this.setState({...this.state, display: '0', leftnum: leftnum, op: op})
-  }
-
-  eqClick() {
-    const {leftnum, op, display} = this.state
-    if (op !== '') {
-      const rightnum = parseInt(display)
-      var res = 0
-      if (op === '+')
-        res = leftnum + rightnum
-      else if (op === '-')
-        res = leftnum - rightnum
-      else if (op === '*')
-        res = leftnum * rightnum
-      else if (op === '/')
-        res = leftnum / rightnum
-
-      this.setState({...this.state, display:res.toString(), leftnum: 0, op: ''})
+class Main extends React.Component {
+    render() {
+        return (
+            <div className='main'>
+                <img className='logo' src="https://d2o2figo6ddd0g.cloudfront.net/6/w/31cxsxy4no5n2j/Flagonly.jpg" alt="Whitworth University"/>
+                <h1 style={{"textAlign":"center"}}>π-rates Calculator</h1>
+                <table className='calc'>
+                    <tbody>
+                    <tr className='calc'>
+                        <td colSpan="5"><span className="display" id="display">0</span></td>
+                    </tr>
+                    <tr>
+                        <td><button className="number" value='7'>7</button></td>
+                        <td><button className="number" value='8'>8</button></td>
+                        <td><button className="number" value='9'>9</button></td>
+                        <td><button className="op">/</button></td>
+                        <td><button className="op">?</button></td>
+                    </tr>
+                    <tr>
+                        <td><button className="number" value='4'>4</button></td>
+                        <td><button className="number" value='5'>5</button></td>
+                        <td><button className="number" value='6'>6</button></td>
+                        <td><button className="op">*</button></td>
+                        <td><button className="op">?</button></td>
+                    </tr>
+                    <tr>
+                        <td><button className="number" value='1'>1</button></td>
+                        <td><button className="number" value='2'>2</button></td>
+                        <td><button className="number" value='3'>3</button></td>
+                        <td><button className="op">-</button></td>
+                        <td><button className="op">?</button></td>
+                    </tr>
+                    <tr>
+                        <td><button className="number" value='0'>0</button></td>
+                        <td><button className="op">CE</button></td>
+                        <td><button className="op">=</button></td>
+                        <td><button className="op">+</button></td>
+                        <td><button className="op">?</button></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
     }
-  }
-
-  render() {
-    const { display } = this.state
-    const numberClick = this.numberClick.bind(this)
-    const ceClick = this.ceClick.bind(this)
-    const opClick = this.opClick.bind(this)
-    const eqClick = this.eqClick.bind(this)
-  
-    return(
-      <div class = 'center'>
-        <img src = 'calc-fav.png' class = 'calc-pic'></img>
-        <p class = 'title'>REACT<br></br>CALC</p>
-
-        <div class = 'calc-box'>
-          <div class="display-bg">
-            <td colspan="5"><span className="display" id="display">{display}</span></td>
-					</div>
-          <table>
-            <tr>
-            <td><button class="number" onClick={numberClick} value='7'>7</button></td>
-              <td><button class="number" onClick={numberClick} value='8'>8</button></td>
-              <td><button class="number" onClick={numberClick} value='9'>9</button></td>
-              <td><button class="op" onClick={opClick} value = '+'>+</button></td>
-            </tr>
-            <tr>
-              <td><button class="number" onClick={numberClick} value='4'>4</button></td>
-              <td><button class="number" onClick={numberClick} value='5'>5</button></td>
-              <td><button class="number" onClick={numberClick} value='6'>6</button></td>
-              <td><button class="op" onClick={opClick} value = '-'>-</button></td>
-            </tr>
-            <tr>
-              <td><button class="number" onClick={numberClick} value='1'>1</button></td>
-              <td><button class="number" onClick={numberClick} value='2'>2</button></td>
-              <td><button class="number" onClick={numberClick} value='3'>3</button></td>
-              <td><button class="op" onClick={opClick} value = '*'>*</button></td>
-            </tr>
-            <tr>
-              <td><button class="number" onClick={numberClick} value='0'>0</button></td>
-              <td><button class="ce" onClick={ceClick}>CE</button></td>
-              <td><button class="eq" onClick={eqClick}>=</button></td>
-              <td><button class="op" onClick={opClick} value = '/'>/</button></td>
-            </tr>
-          </table>
-        </div>
-      </div>
-    )
-  }
-
-  
 }
 
-//wait, what's the difference between App.js and index.js??? do i even need app.js???
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Main />)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Main user='Pete' />);
