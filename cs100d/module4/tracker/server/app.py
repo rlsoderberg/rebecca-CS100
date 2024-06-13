080e976367ede7c8af8cdc6248d6240955d3f990
Rebecca Soderberg <vogindex@gmail.com>
2024-06-13 07:49:29 -0700

    well, it still can't find requirements.txt, not sure what to do about that. that is causing no issues YET... also, seems to have trouble finding new pages??? oh wait, it started working when i put both pages in the same file. that actually makes complete sense...

---
+++ cs100d/module4/tracker/server/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import os
+import pymysql
+
+app = Flask(__name__)
+CORS(app)
+
+#Test API
+@app.route('/')
+def index():
+    return 'WELCOME TO BROWSER 5000'
+
+# Reset DB
+@app.route('/resetdb')
+def reset():
+    #make sure you modify this connection string to connect to your database, not mine
+    conn = pymysql.connect(host='localhost', user='root', password='2101', database='northwind')
+    conn.autocommit(True)
+    crsr = conn.cursor()
+
+    #drop the tables if they already exist
+    sql = 'DROP TABLE IF EXISTS `tracker`.`login`;'
+    crsr.execute(sql)
+    sql = 'DROP TABLE IF EXISTS `tracker`.`user`;'
+    crsr.execute(sql)
+
+    #create the two tables we'll need for our app
+    sql = 'CREATE TABLE `tracker`.`user` (`id` INT NOT NULL AUTO_INCREMENT, `login` VARCHAR(255) NULL, PRIMARY KEY (`id`));'
+    crsr.execute(sql)
+    sql = 'CREATE TABLE `tracker`.`login` (`id` INT NOT NULL AUTO_INCREMENT, `userid` INT NULL, `date` DATETIME, PRIMARY KEY (`id`), FOREIGN KEY (userid) REFERENCES `user`(id));'
+    crsr.execute(sql)
+
+    return 'Reset Successful'
+
+
+if __name__ == '__main__':
+    app.run()
