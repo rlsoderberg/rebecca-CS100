bf155897432c0322812ef35859b218d70e65bedd
Rebecca Soderberg <vogindex@gmail.com>
2024-06-14 21:04:01 -0700

    oh, you actually have to name it .env!!! ok, i copied login, but i don't really get it... first of all, i don't get this json thing!!! like... does it get user from the pymysql.connect somehow??? and i see jsonify is... a flask thing? and what's the deal with these parentheses? so i have to send it through react now? i'm doing this later!!!

--- cs100d/module4/tracker/server/app.py
+++ cs100d/module4/tracker/server/app.py
@@ -38,6 +38,51 @@ def reset():
 
     return 'Reset Successful'
 
+#the user's login is being sent via post, in json
+@app.route('./login', methods = ['POST'])
+def login():
+    server = os.environ['DATAHOST']
+    user = os.environ['DATAUSER']
+    pwd = os.environ['DATAPWD']
+    db = os.environ['DATADATABASE']
+
+    conn = pymysql.connect(host=server, user=user, password=pwd, database=db)
+    conn.autocommit(True)
+    crsr = conn.cursor()
+
+    json = request.get_json()
+    user = json['user']
+
+    #First, check if this user already exists
+    sql = 'select id from user where login=%s'
+    crsr.execute(sql, (user))
+    print('returned ' + str(crsr.rowcount) + ' rows')
+    if crsr.rowcount == 0:
+        print('adding ' + user)
+        crsr.execute('insert into user (login) values (%s)', (user))
+        print('adding ' + str(crsr.rowcount) + ' user')
+        print('re-executing ' + sql)
+        crsr.execute(sql, (user))
+    res = crsr.fetchone()
+    userid = res[0]
+    # Now, add the login information
+    # Note, CURRENT_TIMESTAMP is built into MySQL to get the current time
+    sql = 'insert into login (userid, `date`) values (%S, CURRENT_TIMESTAMP)'
+    crsr.execute(sql, (userid))
+
+    conn.commit()
+
+    # Finally, get the user's login count and the total login count
+    sql = 'select count(*) as logins from login where userid=%s'
+    crsr.execute(sql, (userid))
+    res = crsr.fetchone()
+    usercount = res[0]
+    sql = 'select count(*) as logins from login'
+    crsr.execute(sql)
+    res = crsr.fetchone()
+    totalcount = res[0]
+
+    return jsonify({'user': user, 'user count':usercount, 'total count':totalcount})
 
 if __name__ == '__main__':
     app.run()
