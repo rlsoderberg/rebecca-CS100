cfc8201e3c7d3da8e023952233ec9ee4c9c32601
Rebecca Soderberg <vogindex@gmail.com>
2024-07-03 08:53:11 -0700

    I thought about deleting olds, and that was pointless because all I had to do was reinstall VS Code.(I did end up putting the olds in cool named folders though! Ooh!)
    Reinstalling was startlingly easy, like, I didn't have to re hook it up with Github or anything. It's like, did I actually just reinstall it??? I mean, that was really convenient. Let me try gitignoring my .env, that's the real question.
    
    That worked, but that .env is like, a weird remnant of my slideshow app I thought about making? I think? Anyway, .gitignore is an actual file that exists in the folder, remember that!
    
    Anyway, I was trying to get slideshow.py working, I tried encoding and actually using the filename. Now look, I'm getting the same error, even though I commented most of the code out!!! What is going on???
    
    Oh, so maybe all that stuff was dependent on Flask. Because I put the flask stuff in, and I got a different error.
    
    OK, I tried using get, which is a crazy to me, because I didn't have to use it up in server! But down here, it opens up a whole new can of errors, sooo.... um, it says that it's using the password NO. Is that just from the default stuff it puts in from get? I guess I have to figure out why the original isn't working...

--- cs100d/module4/tracker/tracker-app/src/slideshow.py
+++ cs100d/module4/tracker/tracker-app/src/slideshow.py
@@ -1,15 +1,20 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import os
+import pymysql
+
+app = Flask(__name__)
+CORS(app)
+
 f = open("img_data.txt", "r", encoding='utf-8')
 lines = f.read().splitlines()
 f.close
 
-import os
-import pymysql
-
 def login():
-    server = os.environ['DATAHOST']
-    user = os.environ['DATAUSER']
-    pwd = os.environ['DATAPWD']
-    db = os.environ['DATADATABASE']
+    server = os.environ.get('DATAHOST')
+    user = os.environ.get('DATAUSER')
+    pwd = os.environ.get('DATAPWD')
+    db = os.environ.get('DATADATABASE')
 
     conn = pymysql.connect(host=server, user=user, password=pwd, database=db)
     conn.autocommit(True)
@@ -24,15 +29,18 @@ def loadpic():
     line2 = lines[x+2]
     line3 = lines[x+3]
     line4 = lines[x+4]
-    line5 = f"(LOAD_FILE('C:\Users\rlsod\rebeccaCS100\cs100d\module4\tracker\tracker-app\src\popdecades\{lines[x+5]}', 'rb')"
+    #line5 = f"(LOAD_FILE('C:\Users\rlsod\rebeccaCS100\cs100d\module4\tracker\tracker-app\src\popdecades\{line0}', 'rb', encoding='utf-8')"
+
 
     sql = f'INSERT INTO img (filename, decade, copyright, info, title, photo) VALUES ({line0}, {line1}, {line2}, {line3}, {line4}, {line5});'
 
-    crsr.execute(line1, line2, line3, line4, line5, (user))
+
+    #crsr.execute(line1, line2, line3, line4, line5, (user))
+
 
     conn.commit()
 
 login()
 for x in range(0, 200, 6):
   loadpic()
-  
+
