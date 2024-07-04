52927bcf12c475523fcba25eef2881af57a60e9b
Rebecca Soderberg <vogindex@gmail.com>
2024-07-04 08:31:22 -0700

    I had originally been thinking about using blobs for my images. Truth is, that probably doesn't fit with the approach of reading image data instead. Can I maybe change the way I think about storing images in SQL? Use binary data instead of blobs? What even is a blob? And what is the right format for binary data?
    I tried using varbinary(8000), but it still says my data is too long for column 'photo'!  Maybe my photos are just too big? I mean, I do like large photos. Let's see, my first photo is 105KB, apparently that's bigger than blob or varbinary. I'm going to try longblob.
    
    OK, different, shorter error now! It's still complaining about not being able to find 'C:\\...\\1800s'. LIke, I don't get that. You have trouble opening the img file, so then you have trouble finding the next thing???
    
    Well, whatever. It looks like the thing it's having trouble with is 'img_file = open(img_path, 'rb')'.
    
    No changes to code, so I'd have to do an empty commit. Last time I tried an empty commit, it didn't show up AT ALL! I'm going to try... Well, oh, I better comment this, I mean, I better comment a lot of things...
    
    Now there's an 'incorrect string value: \\xFF\\xD8\\xFF\\xDB\\x00C... for column filename'. Isn't that weird looking? Why are all those double slashes in there?

--- cs100d/module4/tracker/tracker-app/src/slideshow.py
+++ cs100d/module4/tracker/tracker-app/src/slideshow.py
@@ -1,10 +1,12 @@
 import os
 import pymysql
 
+#read image data
 data_file = open("img_data.txt", "r", encoding='utf-8')
 lines = data_file.read().splitlines()
 data_file.close
 
+#log into sql (really should be using .env, but the import was having some weird problem)
 def login():
     server = "localhost"
     user = "root"
@@ -16,9 +18,11 @@ def login():
     crsr = conn.cursor()
     return crsr, conn
 
+#load image data into SQL
 def loadpic(crsr, conn, x):
-    line0 = lines[x]
-    print('line 0:'+line0)
+    #assign variables to different lines of data file
+    img_filename = lines[x]
+    print('img_filename: '+img_filename)
     line1 = lines[x+1]
     print('line 1:'+line1)
     line2 = lines[x+2]
@@ -28,21 +32,27 @@ def loadpic(crsr, conn, x):
     line4 = lines[x+4]
     print('line 4:'+line4)
 
-    img_path = fr'C:\Users\rlsod\rebeccaCS100\cs100d\module4\tracker\tracker-app\src\popdecades\{line0}'
+    #make an image path, using the filename (first line of every five lines of data file)
+    img_path = fr'C:\Users\rlsod\rebeccaCS100\cs100d\module4\tracker\tracker-app\src\popdecades\{img_filename}'
 
+    #read binary data from image file at this image path
     img_file = open(img_path, 'rb')
     img_data = img_file.read()
     img_file.close
     
-    sql = """INSERT INTO img (filename, decade, copyright, info, title, photo) VALUES (%s, %s, %s, %s, %s, %s)"""
+    #sql statement to insert variables and image data
+    sql = """INSERT INTO img (filename, decade, copyright, info, title) VALUES (%s, %s, %s, %s, %s)"""
 
-    params = (line0, line1, line2, line3, line4, img_data)
+    #define params and then execute with them
+    params = (img_data, line1, line2, line3, line4)
 
     crsr.execute(sql, params)
 
     conn.commit()
 
 crsr, conn = login()
-for x in range(0, 200, 6):
+#do the loop... every 6 times? do i need to make a placeholder for the image?
+#or, um, not the image... oh i see, i have 'photo' and i have 'image', 
+for x in range(0, 200, 5):
   loadpic(crsr, conn, x)
 
