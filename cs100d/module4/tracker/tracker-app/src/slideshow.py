db577ba81d5259e2c8aa2bf2c69a0c63d860a372
Rebecca Soderberg <vogindex@gmail.com>
2024-07-03 18:04:03 -0700

    Check this out! I'm trying fr to get a raw f string! I think it might have worked!
    So now... there's something happening in mogrify? query = query % self._escape_args(args, conn)
    TypeError: not all arguments converted during string formatting
    ???
    
    I assume there's something wrong with crsr.execute(sql, 'root'), because I used to have (user) where I now have 'root', but then it was complaining about not knowing what (user) was, because that had been defined in my reference to the .env, and for some reason that whole thing wasn't working. Actually, I think I might have an easy solution for this... one sec...
    
    No, look, that didn't work! I even put ALL the variables in the argument of loadpic! Still no dice!
    
    I was wondering if I forgot to use quotation marks... well, that didn't seem to change anything!
    
    I feel like I should get better at debugging. Not that that would necessarily help? Where I am just confused by a weird error? I'll look at it tomorrow!

--- cs100d/module4/tracker/tracker-app/src/slideshow.py
+++ cs100d/module4/tracker/tracker-app/src/slideshow.py
@@ -1,41 +1,36 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
 import os
 import pymysql
-
-app = Flask(__name__)
-CORS(app)
 
 f = open("img_data.txt", "r", encoding='utf-8')
 lines = f.read().splitlines()
 f.close
 
 def login():
-    conn = pymysql.connect(host='localhost', user='root', password='2101', database='img_db')
+    server = "localhost"
+    user = "root"
+    pwd = "2101"
+    db = "img_db"
+
+    conn = pymysql.connect(host=server, user=user, password=pwd, database=db)
     conn.autocommit(True)
     crsr = conn.cursor()
-    return crsr, conn
+    return user, crsr, conn
 
-crsr, conn = login()
-
-def loadpic():
+def loadpic(user, crsr, conn):
     line0 = lines[x]
     line1 = lines[x+1]
     line2 = lines[x+2]
     line3 = lines[x+3]
     line4 = lines[x+4]
-    img = open(f'C:\Users\rlsod\rebeccaCS100\cs100d\module4\tracker\tracker-app\src\popdecades\{line0}', 'rb', encoding='utf-8')
+    img = open(fr'C:\Users\rlsod\rebeccaCS100\cs100d\module4\tracker\tracker-app\src\popdecades\{line0}')
 
+    sql = fr"INSERT INTO img (filename, decade, copyright, info, title, photo) VALUES ('{line0}', '{line1}', '{line2}', '{line3}', '{line4}', '{img}');"
 
-    sql = f'INSERT INTO img (filename, decade, copyright, info, title, photo) VALUES ({line0}, {line1}, {line2}, {line3}, {line4}, {img});'
-
-
-    crsr.execute(sql, 'root')
-
+    crsr.execute(sql, (user))
 
     conn.commit()
 
-login()
+user, crsr, conn = login()
 for x in range(0, 200, 6):
-  loadpic()
+  loadpic(user, crsr, conn)
 
