5b1c82d5d0a459b182b3015ea78e8d652444e851
Rebecca Soderberg <vogindex@gmail.com>
2024-07-04 09:52:05 -0700

    There, that looks a lot more normal in SQL. The strangest thing happened, I noticed that wherever it should have said 'Getty Images', it said 'Getty s'. It was as though the words 'Image' were somehow always replaced with an image in credits. That would have been a crazy trick!!! I don't actually see that happening anywhere online though. ????

--- cs100d/module4/tracker/tracker-app/src/slideshow.py
+++ cs100d/module4/tracker/tracker-app/src/slideshow.py
@@ -21,8 +21,8 @@ def login():
 #load image data into SQL
 def loadpic(crsr, conn, x):
     #assign variables to different lines of data file
-    img_filename = lines[x]
-    print('img_filename: '+img_filename)
+    line0 = lines[x]
+    print('line 0: '+line0)
     line1 = lines[x+1]
     print('line 1:'+line1)
     line2 = lines[x+2]
@@ -31,20 +31,20 @@ def loadpic(crsr, conn, x):
     print('line 3:'+line3)
     line4 = lines[x+4]
     print('line 4:'+line4)
-
+    
     #make an image path, using the filename (first line of every five lines of data file)
-    img_path = fr'C:\Users\rlsod\rebeccaCS100\cs100d\module4\tracker\tracker-app\src\popdecades\{img_filename}'
+    #img_path = fr'C:\Users\rlsod\rebeccaCS100\cs100d\module4\tracker\tracker-app\src\popdecades\{img_filename}'
 
     #read binary data from image file at this image path
-    img_file = open(img_path, 'rb')
-    img_data = img_file.read()
-    img_file.close()
+    #img_file = open(img_path, 'rb')
+    #img_data = img_file.read()
+    #img_file.close()
     
     #sql statement to insert variables and image data
-    sql = """INSERT INTO img (filename, decade, copyright, info, title, photo) VALUES (%s, %s, %s, %s, %s, %s)"""
+    sql = """INSERT INTO img (filename, decade, copyright, info, title) VALUES (%s, %s, %s, %s, %s)"""
 
     #define params and then execute with them
-    params = (img_filename, line1, line2, line3, line4, img_data)
+    params = (line0, line1, line2, line3, line4)
 
     crsr.execute(sql, params)
 
@@ -53,6 +53,6 @@ def loadpic(crsr, conn, x):
 crsr, conn = login()
 #do the loop... every 6 times? do i need to make a placeholder for the image?
 #or, um, not the image... oh i see, i have 'photo' and i have 'image', 
-for x in range(0, 623, 6):
+for x in range(0, 520, 5):
   loadpic(crsr, conn, x)
 
