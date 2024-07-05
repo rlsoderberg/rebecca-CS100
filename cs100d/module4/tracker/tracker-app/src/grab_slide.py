2756e956f7401b12c31db0c739e5d2ae095c05ab
Rebecca Soderberg <vogindex@gmail.com>
2024-07-05 08:33:10 -0700

    I tried doing it one by one, and that at least worked better! But now all my values are 0!

--- cs100d/module4/tracker/tracker-app/src/grab_slide.py
+++ cs100d/module4/tracker/tracker-app/src/grab_slide.py
@@ -19,14 +19,34 @@ def grab_slide(crsr, conn):
     rand = random.randrange(105)
     
     #get all the different variables. what is the most efficient way to do this?
-    sql = """SELECT (column_name, filename, decade, copyright, info, title) FROM img WHERE column_name = %s;"""
+    sql = """SELECT column_name FROM img WHERE column_name = %s;"""
     params = rand
-    (this_column_name, this_filename, this_decade, this_copyright, this_info, this_title) = crsr.execute(sql, params)
-    conn.commit()
+    this_column_name = crsr.execute(sql, params)
+
+    sql = """SELECT filename FROM img WHERE column_name = %s;"""
+    params = rand
+    this_filename = crsr.execute(sql, params)
+
+    sql = """SELECT decade FROM img WHERE column_name = %s;"""
+    params = rand
+    this_decade = crsr.execute(sql, params)
+
+    sql = """SELECT copyright FROM img WHERE column_name = %s;"""
+    params = rand
+    this_copyright = crsr.execute(sql, params)
+
+    sql = """SELECT info FROM img WHERE column_name = %s;"""
+    params = rand
+    this_info = crsr.execute(sql, params)
+
+    sql = """SELECT title FROM img WHERE column_name = %s;"""
+    params = rand
+    this_title = crsr.execute(sql, params)
 
     return (this_column_name, this_filename, this_decade, this_copyright, this_info, this_title)
 
 crsr, conn = login()
 (this_column_name, this_filename, this_decade, this_copyright, this_info, this_title) = grab_slide(crsr, conn)
+print(str(this_column_name) + ' ' + str(this_filename) + ' ' + str(this_decade) + ' ' + str(this_copyright) + ' ' + str(this_info) + ' ' + str(this_title))
 
 
