6434a4c820ee83df947e80744e50f980419f4f7f
Rebecca Soderberg <vogindex@gmail.com>
2024-07-05 08:15:18 -0700

    I didn't actually manually put in my primary keys yet!!! That was the whole point of committing, so I could check!!!
    So... right now it looks like I'm running into a miscount. Could be in my img_data, let me see how it looks on Github...

--- cs100d/module4/tracker/tracker-app/src/fill_database.py
+++ cs100d/module4/tracker/tracker-app/src/fill_database.py
@@ -22,17 +22,19 @@ def login():
 
 #load image data into SQL
 def loadpic(crsr, conn, x):
-    #assign variables to different lines of data file
+    #assign variables to different lines of data file (is there an easy way to do this better?)
     line0 = lines[x]
     print('line 0: '+line0)
     line1 = lines[x+1]
-    print('line 0: '+line1)
+    print('line 1: '+line1)
     line2 = lines[x+2]
-    print('line 0: '+line2)
+    print('line 2: '+line2)
     line3 = lines[x+3]
-    print('line 0: '+line3)
+    print('line 3: '+line3)
     line4 = lines[x+4]
-    print('line 0: '+line4)
+    print('line 4: '+line4)
+    line5 = lines[x+5]
+    print('line 5: '+line5)
     
     #make an image path, using the filename (first line of every five lines of data file)
     #img_path = fr'C:\Users\rlsod\rebeccaCS100\cs100d\module4\tracker\tracker-app\src\popdecades\{img_filename}'
@@ -43,10 +45,10 @@ def loadpic(crsr, conn, x):
     #img_file.close()
     
     #sql statement to insert variables (not image data)
-    sql = """INSERT INTO img (filename, decade, copyright, info, title) VALUES (%s, %s, %s, %s, %s)"""
+    sql = """INSERT INTO img (column_name, filename, decade, copyright, info, title) VALUES (%s, %s, %s, %s, %s, %s)"""
 
     #define params and then execute with them
-    params = (line0, line1, line2, line3, line4)
+    params = (line0, line1, line2, line3, line4, line5)
 
     crsr.execute(sql, params)
 
@@ -54,7 +56,7 @@ def loadpic(crsr, conn, x):
    
 
 crsr, conn = login()
-#do the loop 5 times
-for x in range(0, 521, 5):
+#do the loop 6 times
+for x in range(0, 630, 6):
   loadpic(crsr, conn, x)
 
--- cs100d/module4/tracker/tracker-app/src/grab_slide.py
+++ cs100d/module4/tracker/tracker-app/src/grab_slide.py
@@ -14,20 +14,19 @@ def login():
     crsr = conn.cursor()
     return crsr, conn
 
-def grab_slide():
+def grab_slide(crsr, conn):
     #i reallly ought to make all these length counts computerized, but for now i'm just doing them manually
     rand = random.randrange(105)
     
     #get all the different variables. what is the most efficient way to do this?
-    sql = """SELECT filename FROM img WHERE key = rand;"""
-    this_filename
+    sql = """SELECT (column_name, filename, decade, copyright, info, title) FROM img WHERE column_name = %s;"""
+    params = rand
+    (this_column_name, this_filename, this_decade, this_copyright, this_info, this_title) = crsr.execute(sql, params)
+    conn.commit()
 
+    return (this_column_name, this_filename, this_decade, this_copyright, this_info, this_title)
 
-    this_decade
-    this_copyright
-    this_info
-    this_title
-
-
+crsr, conn = login()
+(this_column_name, this_filename, this_decade, this_copyright, this_info, this_title) = grab_slide(crsr, conn)
 
 
