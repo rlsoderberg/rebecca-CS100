d931bdd314f17999bb529d84c4afcf621e5de0c0
Rebecca Soderberg <vogindex@gmail.com>
2024-07-09 13:00:47 -0700

    I got the login imported. What was the problem before? Did I not have the requirements installed correctly?

--- cs100d/module4/decade_guessr/server/app.py
+++ cs100d/module4/decade_guessr/server/app.py
@@ -6,19 +6,34 @@ import pymysql
 app = Flask(__name__)
 CORS(app)
 
+#test API
 @app.route('/')
 def index():
     return 'Hello World'
 
-@app.route('/data_test')
-def data_test():
-    conn = pymysql.connect(host='localhost', user='root', password = '2101', database = 'img_db')
+@app.route('/resetdb')
+def reset():
+    server = os.environ['DATAHOST']
+    user = os.environ['DATAUSER']
+    pwd = os.environ['DATAPWD']
+    db = os.environ['DATADATABASE']
+    
+    conn = pymysql.connect(host = server, user = user, password = pwd, database = db)
     conn.autocommit(True)
     crsr = conn.cursor()
 
-    #crsr.execute("SELECT filename, decade, copyright, info, title FROM img WHERE ID = %s" % rand)
-    crsr.execute("SELECT filename, decade, copyright, info, title FROM img WHERE ID = 000")
-    result_row = crsr.fetchone()
+    #drop the tables if they already exist
+    sql = 'DROP TABLE IF EXISTS `tracker`.`login`;'
+    crsr.execute(sql)
+    sql = 'DROP TABLE IF EXISTS `tracker`.`user`;'
+    crsr.execute(sql)
+    #create the two tables we'll need for our app
+    sql = 'CREATE TABLE `tracker`.`user` (`id` INT NOT NULL AUTO_INCREMENT, `login` VARCHAR (255) NULL, PRIMARY KEY (`id`));'
+    crsr.execute(sql)
+    sql = 'CREATE TABLE `tracker`.`login` (`id` INT NOT NULL AUTO_INCREMENT, `userid` INT NULL, `date` DATETIME, PRIMARY KEY (`id`), FOREIGN KEY (userid) REFERENCES `user`(id));'
+    crsr.execute(sql)
+
+    return 'Reset Successful'
 
     return result_row
 
