539226ef527bf3cc408012cea912e961a684eef2
Rebecca Soderberg <vogindex@gmail.com>
2024-07-13 20:19:48 -0700

    Look at this!!! I deleted all of my column contents from the HTML, but they're STILL THERE! I'm going to try reverting to a previous version.
    You can also see, I got frustrated and created a Vue project, but I would probably have similar HTML problems there...

--- cs100d/module4/app_prototype/app_prototype/src/index.js
+++ cs100d/module4/app_prototype/app_prototype/src/index.js
@@ -9,26 +9,12 @@ class Main extends React.Component {
         this.state = {user: null, usercount: 0, totalcount: 0}
         this.urlbase = 'http://127.0.0.1:5000'
     }
-    burgerselector (){
-        document.querySelector('.hamburger-button').addEventListener('click', function() {
-            document.querySelector('.nav-links').style.display = 
-            (document.querySelector('.nav-links').style.display == 'none') ? 'block' : 'none';
-        });
-    }
     render() {
         const {user, usercount, totalcount} = this.state
         return (
             <div className='Main'>
-                <div className = 'Column1'>
-                        <button class="hamburger-button"><img className='burger'src="https://i.ibb.co/ZXJNKSv/smol-pancake.png"></img> </button>
-                        <div className = 'intro-text'>
-                            <p className = 'name'>Becca Soderberg</p>
-                            <p className = 'field'>User Experience and Visual Design</p>
-                            <button class="work-button">Let's Work Together</button>
-                            <p className = 'links'>Email me or find me on Linkedin<br></br>Download my resume</p>
-                        </div>
-                </div>
-                <div className = 'Column2'>Column2</div>
+                <div className = 'Column1'></div>
+                <div className = 'Column2'></div>
             </div>
         )
     }
