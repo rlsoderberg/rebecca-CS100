2265f7ef6731ed046afceef17cab2e225e046bff
Rebecca Soderberg <vogindex@gmail.com>
2024-07-22 19:53:37 -0700

    I'm still not importing these JSONS right! Oh well, I'll skip that, and work on the actual frontend of the program right now...
    Well, it's not really working!!! First I just want to do a slideshow, right? I'm not sure how to hook it up so it renders multiple times. Not to mention dealing with the variables!!!

--- cs100d/module4/app_prototype/app_prototype/src/index.js
+++ cs100d/module4/app_prototype/app_prototype/src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+
+//i don't understand how this is working!!! it says File data/db.json not found, so i'm guessing it's not working?
+//const [images, setImages] = useState([])
+//i tried this and this didn't work either! 
+//import imageData from './data/db.json';
+//i tried json-loader, with this next line, and webpack.config.json, but that didn't work
+//const json = require('json-loader!./data/db.json');
+
+//so all my attempts to load the json from the file aren't working!!! i'm just putting it all in here for now...
+//const obj = JSON.parse('{"images":[{"id":1,"filename":"1843.jpg","decade":"1840s","source":"The Metropolitan Museum of Art","info":"Former U.S. president John Quincy Adams - early dagguerotype by Philip Haas.","title":"First Presidential Photograph"},{"id":2,"filename":"1855.jpg","decade":"1850s","source":"Royal Collection Trust","info":"A British colonel, and two Russian boys, apparently taken prisoner, during the Crimean War - photo by Roger Fenton","title":"Crimean War"},{"id":3,"filename":"1862.jpg","decade":"1860s","source":"The Metropolitan Museum of Art","info":"U.S. President Abraham Lincoln visits Antietam Wood after the Civil Wars Battle of Antietam - photo by Alexander Gardner","title":"Civil War"},{"id":4,"filename":"1877.jpg","decade":"1870s","source":"Library of Congress","info":"Thomas Edison and his early phonograph","title":"Thomas Edison invents phonograph"},{"id":5,"filename":"1884.jpg","decade":"1880s","source":"Library of Congress","info":"Workers build the Statue of Liberty inside French sculptor Frederic Auguste Bartholdis workshop - photo by Pierre Petit","title":"Statue of Liberty constructed"},{"id":6,"filename":"1899p2.jpg","decade":"1890s","source":"Wellcome Collection Gallery","info":"Nikola Tesla sitting in his Colorado Springs laboratory next to his Tesla coil - photo by Dickinson V. Alley","title":"Nikola Tesla - Tesla Coil"},{"id":7,"filename":"1902p1.jpg","decade":"1900s","source":"Breve Storia del Cinema","info":"A frame from the French silent fantasy film 'Le Voyage dans La Lune' by Georges Melies","title":"Le Voyage dans La Lune"},{"id":8,"filename":"1903p1.jpg","decade":"1900s","source":"Library of Congress","info":"First successful flight of the Wright Flyer, by the Wright brothers","title":"Wright Brothers - First Flight"},{"id":9,"filename":"1903p2.jpg","decade":"1900s","source":"Popular Science Monthly Volume 64","info":"Pierre and Maria Skłodowska-Curie in the laboratory with their experimental apparatus, used to detect the ionization of air","title":"Pierre and Marie Curie win Nobel Prize in Physics"},{"id":10,"filename":"1908p1.jpg","decade":"1900s","source":"Library of Congress","info":"Harry Houdini performs a manacled jump from Harvard Bridge in Boston, Massachusetts - photo by John Thurston","title":"Harry Houdini jumps from the Harvard Bridge"},{"id":11,"filename":"1908p2.jpg","decade":"1900s","source":"National Phonograph Co.","info":"Photo of pioneer recording vocal quartet, the
+
+//hey!!!! the quotes and comments and stuff stop working after about 2500 characters (about 12 ids)
+
+const obj = JSON.parse('{"images":[{"id":0,"filename":"1843.jpg","decade":"1840s","source":"The Metropolitan Museum of Art","info":"Former U.S. president John Quincy Adams - early dagguerotype by Philip Haas.","title":"First Presidential Photograph"},{"id":1,"filename":"1862.jpg","decade":"1860s","source":"The Metropolitan Museum of Art","info":"U.S. President Abraham Lincoln visits Antietam Wood - photo by Alexander Gardner","title":"Civil War"},{"id":2,"filename":"1899p2.jpg","decade":"1890s","source":"Wellcome Collection Gallery","info":"Nikola Tesla sitting next to his Tesla coil - photo by Dickinson V. Alley","title":"Nikola Tesla - Tesla Coil"},{"id":3,"filename":"1903p1.jpg","decade":"1900s","source":"Library of Congress","info":"First successful flight of the Wright Flyer, by the Wright brothers","title":"Wright Brothers - First Flight"},{"id":4,"filename":"1936p1.jpg","decade":"1930s","source":"U.S. Farm Security Administration/Office of War Information","info":"Migrant Mother, taken during the Dust Bowl - photo by Dorothea Lange","title":"Dust Bowl"},{"id":5,"filename":"1945p1.jpg","decade":"1940s","source":"National Archives and Records Administration","info":"Slave laborers in the Buchenwald concentration camp are liberated","title":"Holocaust"},{"id":6,"filename":"1969p2.jpg","decade":"1960s","source":"NASA Apollo Archive","info":"Astronaut Edwin E. Aldrin Jr. poses for a photograph during Apollo 11 mission","title":"Apollo 11 Lands on the Moon"},{"id":7,"filename":"1977p2.jpg","decade":"1970s","source":"LucasFilm LTD","info":"Mark Hamill, Carrie Fisher, and Harrison Ford in the film Star Wars Episode IV: A New Hope","title":"Star Wars: A New Hope"},{"id":8,"filename":"1985p2.jpg","decade":"1980s","source":"Universal Pictures","info":"Michael J. Fox and Christopher Lloyd in the film Back to the Future","title":"Back to the Future"},{"id":9,"filename":"1994p1.jpg","decade":"1990s","source":"Columbia","info":"Mariah Careys Merry Christmas promotional photoshoot","title":"Mariah Carey - All I Want For Christmas Is You"},{"id":10,"filename":"2001.jpg","decade":"2000s","source":"National Park Service","info":"September 11 attacks in New York City","title":"9/11 Attacks"},{"id":11,"filename":"2018.jpg","decade":"2010s","source":"UltimateWarrior13","info":"Taylor Swift performing on her Reputation Stadium Tour","title":"Taylor Swift - Shake It Off"}]}')
+
 /*
 from fetch_data import result_row
 
@@ -8,68 +24,41 @@ from fetch_data import result_row
 img_path = fr"cs100d\module4\app_prototype\popdecades\{filename}"
 */
 
+fetch('./data/db.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
 class Main extends React.Component {
     constructor() {
         super()
         //Initial data has no user or counts
         this.state = {filename: null, decade: null, copyright: null, info: null, title: null}
-        this.urlbase = 'http://127.0.0.1:5000'
+        this.urlbase = 'http://localhost:4000'
     }
-    /*
-    resetdb() {
-        axios.get(this.urlbase + '/resetdb').then((resp) => {
-            alert(resp.data)
-        })
-    }
-]   
-    onLoginChange(e) {
-        //Keep track of the login value
-        this.setState({...this.state, user: e.target.value})
-    }
-    login() {
-        const {user} = this.state
-        var url = '/login'
-        // Store the user's name in a JSON object
-        const body = {'user': user}
-        // We're sending JSON data to our server
-        const headers = { "Content-Type": "application/json" }
-        // Configuration information for the server
-        const config = {
-            url: url,
-            baseURL: this.urlbase,
-            method: 'POST',
-            headers: headers,
-            data: body
+
+    getRandFilename(){
+        function randomNumber(min, max) {
+            return Math.random() * (max - min) + min;
         }
-        // Make the request
-        axios(config).then((resp) => {
-            //When this completes, the response from the server has the count data
-            this.setState({...this.state, 
-                usercount: resp.data['user count'], // How many logins for this user?
-                totalcount: resp.data['total count'] //How many total logins?
-            })
-        }).catch(error => {
-            console.log(error)
-        })
+        
+        const rand = randomNumber(0, 11)
+
+        filename = obj[rand].filename
+
+        return rand
     }
-    */
     render() {
-        const {filename, decade, copyright, info, title} = this.state
+        rand = getRandFilename()
         return (
             <div className='Main'>
 
                 <div className = 'img'>
-                    <img src = 'img_path'></img>
+                    <img src = 'cs100d\module4\app_prototype\popdecades\{obj[rand].filename}'></img>
                 </div>
 
                 <div className = 'desc'>
-                    <p>
-                        Filename: {filename}<br />
-                        Decade: {decade}<br />
-                        Copyright: {copyright}<br />
-                        Info: {info}<br />
-                        Title: {title}<br />
-                    </p>
+                    <p>data[rand].title</p>
+                    <button type="button">Next Photo</button>
                 </div>
 
                 
