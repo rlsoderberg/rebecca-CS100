f3168251defa565aebbd4b6df1a381ff963a4717
Rebecca Soderberg <vogindex@gmail.com>
2024-07-24 18:48:39 -0700

    Oh right, I totally spaced and left that 'const obj = obj[rand].parse()' there! Sooo... in order to extract multiple elements from a json... well, first I need to make sure it's parsing right, or whatever.
    So, what inspect says, is 'VM97:1
     Uncaught (in promise)
    SyntaxError: Unexpected token '<', "<!DOCTYPE "... is not valid JSON'. I can't find my invalid JSON!!!

--- cs100d/module4/app_prototype/app_prototype/src/index.js
+++ cs100d/module4/app_prototype/app_prototype/src/index.js
@@ -15,8 +15,14 @@ import './index.css';
 
 //hey!!!! the quotes and comments and stuff stop working after about 2500 characters (about 12 ids)
 
-const obj = JSON.parse('{"images":[{"id":0,"filename":"1843.jpg","decade":"1840s","source":"The Metropolitan Museum of Art","info":"Former U.S. president John Quincy Adams - early dagguerotype by Philip Haas.","title":"First Presidential Photograph"},{"id":1,"filename":"1862.jpg","decade":"1860s","source":"The Metropolitan Museum of Art","info":"U.S. President Abraham Lincoln visits Antietam Wood - photo by Alexander Gardner","title":"Civil War"},{"id":2,"filename":"1899p2.jpg","decade":"1890s","source":"Wellcome Collection Gallery","info":"Nikola Tesla sitting next to his Tesla coil - photo by Dickinson V. Alley","title":"Nikola Tesla - Tesla Coil"},{"id":3,"filename":"1903p1.jpg","decade":"1900s","source":"Library of Congress","info":"First successful flight of the Wright Flyer, by the Wright brothers","title":"Wright Brothers - First Flight"},{"id":4,"filename":"1936p1.jpg","decade":"1930s","source":"U.S. Farm Security Administration/Office of War Information","info":"Migrant Mother, taken during the Dust Bowl - photo by Dorothea Lange","title":"Dust Bowl"},{"id":5,"filename":"1945p1.jpg","decade":"1940s","source":"National Archives and Records Administration","info":"Slave laborers in the Buchenwald concentration camp are liberated","title":"Holocaust"},{"id":6,"filename":"1969p2.jpg","decade":"1960s","source":"NASA Apollo Archive","info":"Astronaut Edwin E. Aldrin Jr. poses for a photograph during Apollo 11 mission","title":"Apollo 11 Lands on the Moon"},{"id":7,"filename":"1977p2.jpg","decade":"1970s","source":"LucasFilm LTD","info":"Mark Hamill, Carrie Fisher, and Harrison Ford in the film Star Wars Episode IV: A New Hope","title":"Star Wars: A New Hope"},{"id":8,"filename":"1985p2.jpg","decade":"1980s","source":"Universal Pictures","info":"Michael J. Fox and Christopher Lloyd in the film Back to the Future","title":"Back to the Future"},{"id":9,"filename":"1994p1.jpg","decade":"1990s","source":"Columbia","info":"Mariah Careys Merry Christmas promotional photoshoot","title":"Mariah Carey - All I Want For Christmas Is You"},{"id":10,"filename":"2001.jpg","decade":"2000s","source":"National Park Service","info":"September 11 attacks in New York City","title":"9/11 Attacks"},{"id":11,"filename":"2018.jpg","decade":"2010s","source":"UltimateWarrior13","info":"Taylor Swift performing on her Reputation Stadium Tour","title":"Taylor Swift - Shake It Off"}]}')
-
+var jsontext = '{"firstname":"Jesper","surname":"Aaberg","phone":["555-0100","555-0120"]}'
+var contact = JSON.parse(jsontext);
+//>>>latest one const obj = JSON.parse('{"id":0,"filename":"1843.jpg","decade":"1840s","source":"The Metropolitan Museum of Art","info":"Former U.S. president John Quincy Adams - early dagguerotype by Philip Haas.","title":"First Presidential Photograph"},{"id":1,"filename":"1862.jpg","decade":"1860s","source":"The Metropolitan Museum of Art","info":"U.S. President Abraham Lincoln visits Antietam Wood - photo by Alexander Gardner","title":"Civil War"},{"id":2,"filename":"1899p2.jpg","decade":"1890s","source":"Wellcome Collection Gallery","info":"Nikola Tesla sitting next to his Tesla coil - photo by Dickinson V. Alley","title":"Nikola Tesla - Tesla Coil"},{"id":3,"filename":"1903p1.jpg","decade":"1900s","source":"Library of Congress","info":"First successful flight of the Wright Flyer, by the Wright brothers","title":"Wright Brothers - First Flight"},{"id":4,"filename":"1936p1.jpg","decade":"1930s","source":"U.S. Farm Security Administration/Office of War Information","info":"Migrant Mother, taken during the Dust Bowl - photo by Dorothea Lange","title":"Dust Bowl"},{"id":5,"filename":"1945p1.jpg","decade":"1940s","source":"National Archives and Records Administration","info":"Slave laborers in the Buchenwald concentration camp are liberated","title":"Holocaust"},{"id":6,"filename":"1969p2.jpg","decade":"1960s","source":"NASA Apollo Archive","info":"Astronaut Edwin E. Aldrin Jr. poses for a photograph during Apollo 11 mission","title":"Apollo 11 Lands on the Moon"},{"id":7,"filename":"1977p2.jpg","decade":"1970s","source":"LucasFilm LTD","info":"Mark Hamill, Carrie Fisher, and Harrison Ford in the film Star Wars Episode IV: A New Hope","title":"Star Wars: A New Hope"},{"id":8,"filename":"1985p2.jpg","decade":"1980s","source":"Universal Pictures","info":"Michael J. Fox and Christopher Lloyd in the film Back to the Future","title":"Back to the Future"},{"id":9,"filename":"1994p1.jpg","decade":"1990s","source":"Columbia","info":"Mariah Careys Merry Christmas promotional photoshoot","title":"Mariah Carey - All I Want For Christmas Is You"},{"id":10,"filename":"2001.jpg","decade":"2000s","source":"National Park Service","info":"September 11 attacks in New York City","title":"9/11 Attacks"},{"id":11,"filename":"2018.jpg","decade":"2010s","source":"UltimateWarrior13","info":"Taylor Swift performing on her Reputation Stadium Tour","title":"Taylor Swift - Shake It Off"}')
+//const obj = JSON.parse('{"images":[{"id":0,"filename":"1843.jpg","decade":"1840s","source":"The Metropolitan Museum of Art","info":"Former U.S. president John Quincy Adams - early dagguerotype by Philip Haas.","title":"First Presidential Photograph"},{"id":1,"filename":"1862.jpg","decade":"1860s","source":"The Metropolitan Museum of Art","info":"U.S. President Abraham Lincoln visits Antietam Wood - photo by Alexander Gardner","title":"Civil War"},{"id":2,"filename":"1899p2.jpg","decade":"1890s","source":"Wellcome Collection Gallery","info":"Nikola Tesla sitting next to his Tesla coil - photo by Dickinson V. Alley","title":"Nikola Tesla - Tesla Coil"},{"id":3,"filename":"1903p1.jpg","decade":"1900s","source":"Library of Congress","info":"First successful flight of the Wright Flyer, by the Wright brothers","title":"Wright Brothers - First Flight"},{"id":4,"filename":"1936p1.jpg","decade":"1930s","source":"U.S. Farm Security Administration/Office of War Information","info":"Migrant Mother, taken during the Dust Bowl - photo by Dorothea Lange","title":"Dust Bowl"},{"id":5,"filename":"1945p1.jpg","decade":"1940s","source":"National Archives and Records Administration","info":"Slave laborers in the Buchenwald concentration camp are liberated","title":"Holocaust"},{"id":6,"filename":"1969p2.jpg","decade":"1960s","source":"NASA Apollo Archive","info":"Astronaut Edwin E. Aldrin Jr. poses for a photograph during Apollo 11 mission","title":"Apollo 11 Lands on the Moon"},{"id":7,"filename":"1977p2.jpg","decade":"1970s","source":"LucasFilm LTD","info":"Mark Hamill, Carrie Fisher, and Harrison Ford in the film Star Wars Episode IV: A New Hope","title":"Star Wars: A New Hope"},{"id":8,"filename":"1985p2.jpg","decade":"1980s","source":"Universal Pictures","info":"Michael J. Fox and Christopher Lloyd in the film Back to the Future","title":"Back to the Future"},{"id":9,"filename":"1994p1.jpg","decade":"1990s","source":"Columbia","info":"Mariah Careys Merry Christmas promotional photoshoot","title":"Mariah Carey - All I Want For Christmas Is You"},{"id":10,"filename":"2001.jpg","decade":"2000s","source":"National Park Service","info":"September 11 attacks in New York City","title":"9/11 Attacks"},{"id":11,"filename":"2018.jpg","decade":"2010s","source":"UltimateWarrior13","info":"Taylor Swift performing on her Reputation Stadium Tour","title":"Taylor Swift - Shake It Off"}');
+//just checking whether i put in too many quotation marks. i don't think that's it???
+//const obj = JSON.parse('{images:[{id:0,filename:"1843.jpg",decade:"1840s",source:"The Metropolitan Museum of Art",info:"Former U.S. president John Quincy Adams - early dagguerotype by Philip Haas.",title:"First Presidential Photograph"},{id:1,filename:"1862.jpg",decade:"1860s",source:"The Metropolitan Museum of Art",info:"U.S. President Abraham Lincoln visits Antietam Wood - photo by Alexander Gardner",title:"Civil War"}]};') 
+//const obj = JSON.parse('{"name":"John", "age":30, "city":"New York"}');
+//person = {name:"John", age:31, city:"New York"};
 /*
 from fetch_data import result_row
 
@@ -41,11 +47,19 @@ class Main extends React.Component {
             return Math.random() * (max - min) + min;
         }
 
-        const rand = getRandomInt(11)
+        var rand = getRandomInt(11)
         
         this.setState({...this.state, rand: rand})
 
-        const obj = obj[rand].parse()
+        //const obj = obj[rand].parse()
+/* not even going to finish this one!!!
+        var id=a.getEmailBasicData;
+        var filename=a.getEmailDocumentData;
+        var decade=a.getEmailParticipantData;
+        var source=a.getEmailBasicData;
+        var info=a.getEmailDocumentData;
+        var title=a.getEmailParticipantData;
+        */
 /*
         (id, filename, decade, source, info, title) = obj[rand]
 
@@ -54,6 +68,9 @@ class Main extends React.Component {
     }
     render() {
         const {filename, title} = this.state
+        var str = JSON.stringify(contact);
+        var id = str[1]
+        console.log(id)
         return (
             <div className='Main'>
 
